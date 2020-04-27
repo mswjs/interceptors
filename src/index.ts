344fc8b5a030d7103426c8d177a0a43e0ed54132
Artem Zakharchenko <kettanaito@gmail.com>
2020-04-27 16:41:36 +0200

    Replaces XMLHttpRequest if run in a browser-like environment

--- src/index.ts
+++ src/index.ts
@@ -2,19 +2,28 @@ import http from 'http'
 import https from 'https'
 import { RequestHandler } from './glossary'
 import { overrideHttpModule } from './ClientRequest/overrideHttpModule'
+import { createXMLHttpRequestOverride } from './XMLHttpRequest/XMLHttpRequestOverride'
 
 const httpRequestCopy = http.request
 const httpGetCopy = http.get
 const httpsRequestCopy = https.request
 const httpsGetCopy = https.get
+const xmlHttpRequestCopy = window?.XMLHttpRequest
 
 export class RequestInterceptor {
+  private isBrowserLikeEnvironment: boolean
   private handlers: RequestHandler[]
 
   constructor() {
+    this.isBrowserLikeEnvironment = typeof window !== 'undefined'
     this.handlers = []
 
     overrideHttpModule(this.handleRequest)
+
+    if (this.isBrowserLikeEnvironment) {
+      // @ts-ignore
+      window.XMLHttpRequest = createXMLHttpRequestOverride(this.handleRequest)
+    }
   }
 
   /**
@@ -26,6 +35,10 @@ export class RequestInterceptor {
 
     https.request = httpsRequestCopy
     https.get = httpsGetCopy
+
+    if (this.isBrowserLikeEnvironment) {
+      window.XMLHttpRequest = xmlHttpRequestCopy
+    }
   }
 
   /**
---
+++ test/xhr.test.ts
@@ -0,0 +1,54 @@
+import { RequestInterceptor } from '../src'
+
+describe('XHR', () => {
+  let interceptor: RequestInterceptor
+
+  beforeAll(() => {
+    interceptor = new RequestInterceptor()
+    interceptor.use((req) => {
+      if (req.url === 'https://test.msw.io') {
+        return {
+          status: 301,
+          headers: {
+            'Content-Type': 'application/hal+json',
+          },
+          body: 'foo',
+        }
+      }
+    })
+  })
+
+  afterAll(() => {
+    interceptor.restore()
+  })
+
+  describe('given I perform an XMLHttpRequest', () => {
+    let status: number
+    let headers: string
+    let body: string
+
+    beforeAll((done) => {
+      const req = new XMLHttpRequest()
+      req.onload = () => {
+        body = req.response
+        headers = req.getAllResponseHeaders()
+        status = req.status
+        done()
+      }
+      req.open('GET', 'https://test.msw.io')
+      req.send()
+    })
+
+    it('should return mocked status code', () => {
+      expect(status).toEqual(301)
+    })
+
+    it('should return mocked body', () => {
+      expect(body).toEqual('foo')
+    })
+
+    it('should return mocked headers', () => {
+      expect(headers).toContain('Content-Type: application/hal+json')
+    })
+  })
+})
