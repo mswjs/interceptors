33a4ae876baf469416c8feca5521e0a1a9e5b0fd
Artem Zakharchenko <kettanaito@gmail.com>
2020-05-01 15:05:08 +0200

    XMLHttpRequest: Check if window exists before storing the original reference

--- src/XMLHttpRequest/override.ts
+++ src/XMLHttpRequest/override.ts
@@ -2,15 +2,14 @@ import { ModuleOverride } from '../glossary'
 import { createXMLHttpRequestOverride } from './XMLHttpRequest/XMLHttpRequestOverride'
 
 const original = {
-  XMLHttpRequest: window?.XMLHttpRequest,
+  XMLHttpRequest:
+    // Although executed in node, certain processes emulate the DOM-like environment
+    // (i.e. `js-dom` in Jest). The `window` object would be avilable in such environments.
+    typeof window === 'undefined' ? undefined : window.XMLHttpRequest,
 }
 
 export const overrideXhrModule: ModuleOverride = (middleware) => {
-  // Although executed in node, certain processes emulate the DOM-like environment
-  // (i.e. `js-dom` in Jest). The `window` object would be avilable in such environments.
-  const isBrowserLikeEnvironment = typeof window !== 'undefined'
-
-  if (isBrowserLikeEnvironment) {
+  if (original.XMLHttpRequest) {
     const XMLHttpRequestOverride = createXMLHttpRequestOverride(
       middleware,
       original.XMLHttpRequest
@@ -21,7 +20,7 @@ export const overrideXhrModule: ModuleOverride = (middleware) => {
   }
 
   return () => {
-    if (isBrowserLikeEnvironment) {
+    if (original.XMLHttpRequest) {
       window.XMLHttpRequest = original.XMLHttpRequest
     }
   }
