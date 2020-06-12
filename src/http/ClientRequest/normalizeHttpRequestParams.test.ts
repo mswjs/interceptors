89057090b8162f1d138d9f53253a5e7e9c1629cb
Artem Zakharchenko <kettanaito@gmail.com>
2020-06-12 10:01:12 +0200

    http (normalize): Supports partial RequestOptions instance (no "protocol" and "hostname")

---
+++ src/http/ClientRequest/normalizeHttpRequestParams.test.ts
@@ -0,0 +1,148 @@
+import { normalizeHttpRequestParams } from './normalizeHttpRequestParams'
+
+test('handles [string, callback] input', () => {
+  const [
+    url,
+    options,
+    callback,
+  ] = normalizeHttpRequestParams('https://mswjs.io/resource', function cb() {})
+
+  // URL string must be converted to a URL instance
+  expect(url.toJSON()).toEqual(new URL('https://mswjs.io/resource').toJSON())
+
+  // Request options must be derived from the URL instance
+  expect(options).toHaveProperty('method', 'GET')
+  expect(options).toHaveProperty('protocol', 'https:')
+  expect(options).toHaveProperty('hostname', 'mswjs.io')
+  expect(options).toHaveProperty('path', '/resource')
+
+  // Callback must be preserved
+  expect(callback).toHaveProperty('name', 'cb')
+})
+
+test('handles [string, RequestOptions, callback] input', () => {
+  const initialOptions = {
+    headers: {
+      'Content-Type': 'text/plain',
+    },
+  }
+  const [
+    url,
+    options,
+    callback,
+  ] = normalizeHttpRequestParams(
+    'https://mswjs.io/resource',
+    initialOptions,
+    function cb() {}
+  )
+
+  // URL must be created from the string
+  expect(url.toJSON()).toEqual(new URL('https://mswjs.io/resource').toJSON())
+
+  // Request options must be preserved
+  expect(options).toEqual(initialOptions)
+
+  // Callback must be preserved
+  expect(callback).toHaveProperty('name', 'cb')
+})
+
+test('handles [URL, callback] input', () => {
+  const [url, options, callback] = normalizeHttpRequestParams(
+    new URL('https://mswjs.io/resource'),
+    function cb() {}
+  )
+
+  // URL must be preserved
+  expect(url.toJSON()).toEqual(new URL('https://mswjs.io/resource').toJSON())
+
+  // Request options must be derived from the URL instance
+  expect(options).toHaveProperty('method', 'GET')
+  expect(options).toHaveProperty('protocol', 'https:')
+  expect(options).toHaveProperty('hostname', 'mswjs.io')
+  expect(options).toHaveProperty('path', '/resource')
+
+  // Callback must be preserved
+  expect(callback).toHaveProperty('name', 'cb')
+})
+
+test('handles [URL, RequestOptions, callback] input', () => {
+  const [url, options, callback] = normalizeHttpRequestParams(
+    new URL('https://mswjs.io/resource'),
+    {
+      headers: {
+        'Content-Type': 'text/plain',
+      },
+    },
+    function cb() {}
+  )
+
+  // URL must be preserved
+  expect(url.toJSON()).toEqual(new URL('https://mswjs.io/resource').toJSON())
+
+  // Options must be preserved
+  expect(options).toEqual({
+    protocol: 'https:',
+    headers: {
+      'Content-Type': 'text/plain',
+    },
+  })
+
+  // Callback must be preserved
+  expect(callback).toHaveProperty('name', 'cb')
+})
+
+test('handles [RequestOptions, callback] input', () => {
+  const initialOptions = {
+    method: 'POST',
+    protocol: 'https:',
+    host: 'mswjs.io',
+    path: '/resource',
+    headers: {
+      'Content-Type': 'text/plain',
+    },
+  }
+  const [url, options, callback] = normalizeHttpRequestParams(
+    initialOptions,
+    function cb() {}
+  )
+
+  // URL must be derived from request options
+  expect(url.toJSON()).toEqual(new URL('https://mswjs.io/resource').toJSON())
+
+  // Request options must be preserved
+  expect(options).toEqual(initialOptions)
+
+  // Callback must be preserved
+  expect(callback).toHaveProperty('name', 'cb')
+})
+
+/**
+ * @see https://github.com/mswjs/node-request-interceptor/issues/19
+ */
+test('handles [PartialRequestOptions, callback] input', () => {
+  const initialOptions = {
+    method: 'GET',
+    port: '50176',
+    path: '/resource',
+    host: '127.0.0.1',
+    ca: undefined,
+    key: undefined,
+    pfx: undefined,
+    cert: undefined,
+    passphrase: undefined,
+    agent: false,
+  }
+  const [url, options, callback] = normalizeHttpRequestParams(
+    initialOptions,
+    function cb() {}
+  )
+
+  // URL must be derived from request options
+  expect(url.toJSON()).toEqual(new URL('http://127.0.0.1/resource').toJSON())
+
+  // Request options must be preserved
+  expect(options).toEqual(initialOptions)
+
+  // Callback must be preserved
+  expect(callback).toHaveProperty('name', 'cb')
+})
--- src/utils/urlToOptions.ts
+++ src/utils/urlToOptions.ts
@@ -3,31 +3,25 @@ import { RequestOptions } from 'http'
 /**
  * Converts a URL instance into an ordinary object expected by
  * the `http.request`/`https.request` calls.
- *
  * @see https://github.com/nodejs/node/blob/908292cf1f551c614a733d858528ffb13fb3a524/lib/internal/url.js#L1257
  */
 export function urlToOptions(url: URL): RequestOptions {
-  const options = {
+  const options: RequestOptions = {
+    method: 'GET',
     protocol: url.protocol,
     hostname:
       typeof url.hostname === 'string' && url.hostname.startsWith('[')
         ? url.hostname.slice(1, -1)
         : url.hostname,
-    hash: url.hash,
-    search: url.search,
-    pathname: url.pathname,
+    host: url.host,
     path: `${url.pathname}${url.search || ''}`,
-    href: url.href,
-    method: 'GET',
   }
 
-  if (url.port !== '') {
-    // @ts-ignore
+  if (!!url.port) {
     options.port = Number(url.port)
   }
 
   if (url.username || url.password) {
-    // @ts-ignore
     options.auth = `${url.username}:${url.password}`
   }
 
