a91ad074885238eb702499d36b8f80d8d2a90a6f
Artem Zakharchenko <kettanaito@gmail.com>
2020-09-09 09:51:44 +0200

    Uses internal server for interception tests

--- test/intercept/http/http.get.test.ts
+++ test/intercept/http/http.get.test.ts
@@ -5,11 +5,19 @@ import { RequestInterceptor } from '../../../src'
 import withDefaultInterceptors from '../../../src/presets/default'
 import { InterceptedRequest } from '../../../src/glossary'
 import { httpGet, prepare } from '../../helpers'
+import { ServerAPI, createServer } from '../../utils/createServer'
 
 let requestInterceptor: RequestInterceptor
 let pool: InterceptedRequest[] = []
+let server: ServerAPI
 
-beforeAll(() => {
+beforeAll(async () => {
+  server = await createServer((app) => {
+    app.get('/user', (req, res) => {
+      res.status(200).send('user-body').end()
+    })
+  })
+
   requestInterceptor = new RequestInterceptor(withDefaultInterceptors)
   requestInterceptor.use((req) => {
     pool.push(req)
@@ -20,13 +28,14 @@ afterEach(() => {
   pool = []
 })
 
-afterAll(() => {
+afterAll(async () => {
   requestInterceptor.restore()
+  await server.close()
 })
 
 test('intercepts an http.get request', async () => {
   const request = await prepare(
-    httpGet('http://httpbin.org/get?userId=123', {
+    httpGet(server.makeHttpUrl('/user?id=123'), {
       headers: {
         'x-custom-header': 'yes',
       },
@@ -36,8 +45,8 @@ test('intercepts an http.get request', async () => {
 
   expect(request).toBeTruthy()
   expect(request?.url).toBeInstanceOf(URL)
-  expect(request?.url.toString()).toEqual('http://httpbin.org/get?userId=123')
+  expect(request?.url.toString()).toEqual(server.makeHttpUrl('/user?id=123'))
   expect(request).toHaveProperty('method', 'GET')
-  expect(request?.url.searchParams.get('userId')).toEqual('123')
+  expect(request?.url.searchParams.get('id')).toEqual('123')
   expect(request?.headers).toHaveProperty('x-custom-header', 'yes')
 })
