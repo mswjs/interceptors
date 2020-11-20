28ef60c8e50ff9f2846c9d716819d2515a904866
Artem Zakharchenko <kettanaito@gmail.com>
2020-11-20 10:39:29 +0100

    normalizeHttpRequestParams: Formats the file and comments

--- src/interceptors/ClientRequest/utils/normalizeHttpRequestParams.ts
+++ src/interceptors/ClientRequest/utils/normalizeHttpRequestParams.ts
@@ -1,5 +1,5 @@
 import { RequestOptions } from 'https'
-import { Url as LegacyURL } from 'url';
+import { Url as LegacyURL } from 'url'
 import { HttpRequestCallback, RequestSelf } from '../../../glossary'
 import { getRequestOptionsByUrl } from '../../../utils/getRequestOptionsByUrl'
 import { getUrlByRequestOptions } from '../../../utils/getUrlByRequestOptions'
@@ -68,37 +68,42 @@ export function normalizeHttpRequestParams(
 
     callback = resolveCallback(args)
   }
-  // Handle a legacy Url and re-normalize from either a RequestOptions object
-  // or a WHATWG URL
+  // Handle a legacy URL instance and re-normalize from either a RequestOptions object
+  // or a WHATWG URL.
   else if ('hash' in args[0] && !('method' in args[0])) {
-    if (args[0].hostname === null) {
-      /*
-        We are dealing with a relative url, so use the path as an "option" and
-        merge-in any existing options, giving priority to exising options -- i.e. a path in any
-        existing options will take precedence over the one contained in the url. This is consistent
-        with the behaviour in ClientRequest.
-        
-        https://github.com/nodejs/node/blob/d84f1312915fe45fe0febe888db692c74894c382/lib/_http_client.js#L122
-      */
-      debug('given a relative legacy url:', args[0])
+    const [legacyUrl] = args
+
+    if (legacyUrl.hostname === null) {
+      // We are dealing with a relative url, so use the path as an "option" and
+      // merge in any existing options, giving priority to exising options -- i.e. a path in any
+      // existing options will take precedence over the one contained in the url. This is consistent
+      // with the behaviour in ClientRequest.
+      // https://github.com/nodejs/node/blob/d84f1312915fe45fe0febe888db692c74894c382/lib/_http_client.js#L122
+      debug('given a relative legacy URL:', legacyUrl)
 
       return isObject(args[1])
-        ? normalizeHttpRequestParams({path: args[0].path, ...args[1]}, args[2])
-        : normalizeHttpRequestParams({path: args[0].path}, args[1] as HttpRequestCallback)
+        ? normalizeHttpRequestParams(
+            { path: legacyUrl.path, ...args[1] },
+            args[2]
+          )
+        : normalizeHttpRequestParams(
+            { path: legacyUrl.path },
+            args[1] as HttpRequestCallback
+          )
     }
 
-    debug('given an absolute legacy url:', args[0])
+    debug('given an absolute legacy url:', legacyUrl)
 
-    //We are dealing with an absolute url, so convert to WHATWG and try again
-    const resolvedUrl = new URL(args[0].href)
+    // We are dealing with an absolute URL, so convert to WHATWG and try again.
+    const resolvedUrl = new URL(legacyUrl.href)
 
     return args[1] === undefined
       ? normalizeHttpRequestParams(resolvedUrl)
       : typeof args[1] === 'function'
-        ? normalizeHttpRequestParams(resolvedUrl, args[1])
-        : normalizeHttpRequestParams(resolvedUrl, args[1], args[2])
+      ? normalizeHttpRequestParams(resolvedUrl, args[1])
+      : normalizeHttpRequestParams(resolvedUrl, args[1], args[2])
   }
-  // Handle a given request options object as-is
+  // Handle a given RequestOptions object as-is
   // and derive the URL instance from it.
   else if (isObject(args[0])) {
     options = args[0]
