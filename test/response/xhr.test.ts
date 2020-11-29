3cb10197661d1d0b0e343dcc08560158eeff434d
Artem Zakharchenko <kettanaito@gmail.com>
2020-11-29 15:24:56 +0100

    Uses "createXMLHttpRequest" in the response tests

--- test/response/xhr.test.ts
+++ test/response/xhr.test.ts
@@ -1,34 +1,7 @@
 import { RequestInterceptor } from '../../src'
 import withDefaultInterceptors from '../../src/presets/default'
+import { createXMLHttpRequest } from '../helpers'
 import { ServerAPI, createServer } from '../utils/createServer'
-
-interface XhrResponse {
-  status: number
-  headers: string
-  body: string
-}
-
-function performXMLHttpRequest(
-  method: string,
-  url: string
-): Promise<XhrResponse> {
-  return new Promise((resolve, reject) => {
-    const req = new XMLHttpRequest()
-    let status: number
-    let headers: string
-    let body: string
-
-    req.onload = () => {
-      body = req.response
-      headers = req.getAllResponseHeaders()
-      status = req.status
-      resolve({ status, headers, body })
-    }
-    req.onerror = reject
-    req.open(method, url)
-    req.send()
-  })
-}
 
 let interceptor: RequestInterceptor
 let server: ServerAPI
@@ -76,46 +49,61 @@ afterAll(async () => {
 })
 
 test('responds to an HTTP request handled in the middleware', async () => {
-  const res = await performXMLHttpRequest('GET', server.makeHttpUrl('/'))
+  const req = await createXMLHttpRequest((req) => {
+    req.open('GET', server.makeHttpUrl('/'))
+  })
+  const responseHeaders = req.getAllResponseHeaders()
 
-  expect(res.status).toEqual(301)
-  expect(res.headers).toContain('Content-Type: application/hal+json')
-  expect(res.body).toEqual('foo')
+  expect(req.status).toEqual(301)
+  expect(responseHeaders).toContain('Content-Type: application/hal+json')
+  expect(req.response).toEqual('foo')
 })
 
 test('bypasses an HTTP request not handled in the middleware', async () => {
-  const res = await performXMLHttpRequest('GET', server.makeHttpUrl('/get'))
+  const req = await createXMLHttpRequest((req) => {
+    req.open('GET', server.makeHttpUrl('/get'))
+  })
 
-  expect(res.status).toEqual(200)
-  expect(res.body).toEqual('/get')
+  expect(req.status).toEqual(200)
+  expect(req.response).toEqual('/get')
 })
 
 test('responds to an HTTPS request handled in the middleware', async () => {
-  const res = await performXMLHttpRequest('GET', server.makeHttpsUrl('/'))
+  const req = await createXMLHttpRequest((req) => {
+    req.open('GET', server.makeHttpsUrl('/'))
+  })
+  const responseHeaders = req.getAllResponseHeaders()
 
-  expect(res.status).toEqual(301)
-  expect(res.headers).toContain('Content-Type: application/hal+json')
-  expect(res.body).toEqual('foo')
+  expect(req.status).toEqual(301)
+  expect(responseHeaders).toContain('Content-Type: application/hal+json')
+  expect(req.response).toEqual('foo')
 })
 
 test('bypasses an HTTPS request not handled in the middleware', async () => {
-  const res = await performXMLHttpRequest('GET', server.makeHttpsUrl('/get'))
+  const req = await createXMLHttpRequest((req) => {
+    req.open('GET', server.makeHttpsUrl('/get'))
+  })
 
-  expect(res.status).toEqual(200)
-  expect(res.body).toEqual('/get')
+  expect(req.status).toEqual(200)
+  expect(req.response).toEqual('/get')
 })
 
 test('responds to an HTTP request to a relative URL that is handled in the middleware', async () => {
-  const res = await performXMLHttpRequest('POST', '/login')
+  const req = await createXMLHttpRequest((req) => {
+    req.open('POST', server.makeHttpsUrl('/login'))
+  })
+  const responseHeaders = req.getAllResponseHeaders()
 
-  expect(res.status).toEqual(301)
-  expect(res.headers).toContain('Content-Type: application/hal+json')
-  expect(res.body).toEqual('foo')
+  expect(req.status).toEqual(301)
+  expect(responseHeaders).toContain('Content-Type: application/hal+json')
+  expect(req.response).toEqual('foo')
 })
 
 test('produces a request error when the middleware throws an exception', async () => {
   const getResponse = () => {
-    return performXMLHttpRequest('GET', 'https://error.me')
+    return createXMLHttpRequest((req) => {
+      req.open('GET', 'https://error.me')
+    })
   }
 
   // No way to assert the rejection error, because XMLHttpRequest doesn't propagate it.
@@ -124,8 +112,10 @@ test('produces a request error when the middleware throws an exception', async (
 
 test('bypasses any request when the interceptor is restored', async () => {
   interceptor.restore()
-  const res = await performXMLHttpRequest('GET', server.makeHttpsUrl('/'))
+  const req = await createXMLHttpRequest((req) => {
+    req.open('GET', server.makeHttpsUrl('/'))
+  })
 
-  expect(res.status).toEqual(200)
-  expect(res.body).toEqual('/')
+  expect(req.status).toEqual(200)
+  expect(req.response).toEqual('/')
 })
