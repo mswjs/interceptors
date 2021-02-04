1246799e812609090c349ff7614e515d350ab41c
Kalo Pilato <hey@kalopilato.com>
2021-02-04 13:20:59 +1300

    test: add failing test to cover XHR `getResponseHeader` case sensitivity

---
+++ test/regressions/xhr-response-header-case-sensitivity.test.ts
@@ -0,0 +1,40 @@
+import { ServerApi, createServer } from '@open-draft/test-server'
+import { RequestInterceptor } from '../../src'
+import { createXMLHttpRequest } from '../helpers'
+import { interceptXMLHttpRequest } from '../../src/interceptors/XMLHttpRequest'
+
+let server: ServerApi
+let interceptor: RequestInterceptor
+
+beforeAll(async () => {
+  server = await createServer((app) => {
+    app.get('/account', (req, res) => {
+      return res
+        .status(200)
+        .set(
+          'access-control-expose-headers',
+          'x-test-header'
+        )
+        .set('x-test-header', req.get('x-test-header'))
+        .send(null)
+    })
+  })
+
+  interceptor = new RequestInterceptor([interceptXMLHttpRequest])
+})
+
+afterAll(async () => {
+  interceptor.restore()
+  await server.close()
+})
+
+test('getResponseHeader is case insensitive', async () => {
+  const req = await createXMLHttpRequest((req) => {
+    req.open('GET', server.http.makeUrl('/account'))
+    req.setRequestHeader('x-test-header', 'test-value')
+  })
+
+  expect(req.getResponseHeader('x-test-header')).toEqual('test-value')
+  expect(req.getResponseHeader('X-Test-Header')).toEqual('test-value')
+  expect(req.getResponseHeader('X-TEST-HEADER')).toEqual('test-value')
+})
