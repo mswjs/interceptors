d7c47af4ba0a877aba91d6f5460323d5d089cb24
Artem Zakharchenko <kettanaito@gmail.com>
2021-03-08 12:16:49 +0100

    ClientRequest: Infers protocol from the request module (defaultProtocol)

--- test/intercept/https/https.get.test.ts
+++ test/intercept/https/https.get.test.ts
@@ -1,11 +1,13 @@
 /**
  * @jest-environment node
  */
+import * as https from 'https'
 import { ServerApi, createServer, httpsAgent } from '@open-draft/test-server'
 import { createInterceptor } from '../../../src'
 import { IsomoprhicRequest } from '../../../src/createInterceptor'
 import { interceptClientRequest } from '../../../src/interceptors/ClientRequest'
 import { prepare, httpsGet } from '../../helpers'
+import { getIncomingMessageBody } from '../../../src/interceptors/ClientRequest/utils/getIncomingMessageBody'
 
 let pool: IsomoprhicRequest[] = []
 let server: ServerApi
@@ -53,4 +55,26 @@ test('intercepts an HTTPS GET request', async () => {
   expect(request).toHaveProperty('method', 'GET')
   expect(request?.url.searchParams.get('id')).toEqual('123')
   expect(request?.headers).toHaveProperty('x-custom-header', 'yes')
+})
+
+test('intercepts an https.get request given RequestOptions without a protocol', async (done) => {
+  // Pass a RequestOptions object without an explicit `protocol`.
+  // The request is made via `https` so the `https:` protocol must be inferred.
+  const request = https.get(
+    {
+      host: server.https.getAddress().host,
+      port: server.https.getAddress().port,
+      path: '/user',
+      // Suppress the "certificate has expired" error.
+      rejectUnauthorized: false,
+    },
+    async (response) => {
+      const responseBody = await getIncomingMessageBody(response)
+      expect(responseBody).toBe('user-body')
+
+      done()
+    }
+  )
+
+  request.end()
 })
