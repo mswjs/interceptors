d1a5f689d6dc75ad820660ef26abab327b9b9da4
marcosvega91 <marcomoretti0103@gmail.com>
2021-03-08 16:56:41 +0100

    fix(XMLHttpRequest): set readyState to "DONE" on bypassed response
    As for the mocked request also bypass request should have the readyState set to 4 (DONE) when a response is received
    
    fix: https://github.com/mswjs/msw/issues/636

--- test/response/axios.test.ts
+++ test/response/axios.test.ts
@@ -1,6 +1,9 @@
 import axios from 'axios'
+import { ServerApi, createServer } from '@open-draft/test-server'
 import { createInterceptor } from '../../src'
 import nodeInterceptors from '../../src/presets/node'
+
+let server: ServerApi
 
 const interceptor = createInterceptor({
   modules: nodeInterceptors,
@@ -20,12 +23,27 @@ const interceptor = createInterceptor({
   },
 })
 
-beforeAll(() => {
+beforeAll(async () => {
+  server = await createServer((app) => {
+    app.get('/books', (req, res) => {
+      res.status(200).json([
+        {
+          title: 'The Lord of the Rings',
+          author: 'J. R. R. Tolkien',
+        },
+        {
+          title: 'The Hobbit',
+          author: 'J. R. R. Tolkien',
+        },
+      ])
+    })
+  })
   interceptor.apply()
 })
 
-afterAll(() => {
+afterAll(async () => {
   interceptor.restore()
+  await server.close()
 })
 
 test('responds with a mocked response to an "axios()" request', async () => {
@@ -50,4 +68,20 @@ test('responds with a mocked response to an "axios.post()" request', async () =>
   expect(res.status).toEqual(200)
   expect(res.headers).toHaveProperty('x-header', 'yes')
   expect(res.data).toEqual({ mocked: true })
+})
+
+test('bypass the interceptor and return the original response', async () => {
+  const res = await axios.get(server.http.makeUrl('/books'))
+
+  expect(res.status).toEqual(200)
+  expect(res.data).toEqual([
+    {
+      title: 'The Lord of the Rings',
+      author: 'J. R. R. Tolkien',
+    },
+    {
+      title: 'The Hobbit',
+      author: 'J. R. R. Tolkien',
+    },
+  ])
 })
