16fd35622efe6328497ade15e21a84f6afda0a93
Artem Zakharchenko <kettanaito@gmail.com>
2021-04-09 12:01:03 +0200

    Adds "request" event tests

---
+++ test/events/request.test.ts
@@ -0,0 +1,77 @@
+import { createServer, ServerApi } from '@open-draft/test-server'
+import { createInterceptor, IsomorphicRequest } from '../../src'
+import { interceptXMLHttpRequest } from '../../src/interceptors/XMLHttpRequest'
+import { interceptClientRequest } from '../../src/interceptors/ClientRequest'
+import { createXMLHttpRequest, httpRequest } from '../helpers'
+
+let requests: IsomorphicRequest[] = []
+let server: ServerApi
+const interceptor = createInterceptor({
+  modules: [interceptClientRequest, interceptXMLHttpRequest],
+  resolver() {},
+})
+
+interceptor.on('request', (request) => {
+  requests.push(request)
+})
+
+beforeAll(async () => {
+  server = await createServer((app) => {
+    app.post('/user', (req, res) => {
+      res.status(200).end()
+    })
+  })
+
+  interceptor.apply()
+})
+
+afterEach(() => {
+  requests = []
+})
+
+afterAll(async () => {
+  interceptor.restore()
+  await server.close()
+})
+
+it('ClientRequest: emits the "request" event upon a request', async () => {
+  await httpRequest(
+    server.http.makeUrl('/user'),
+    {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    },
+    JSON.stringify({
+      userId: 'abc-123',
+    })
+  )
+  const [request] = requests
+
+  expect(requests).toHaveLength(1)
+  expect(request.method).toEqual('POST')
+  expect(request.url.toString()).toEqual(server.http.makeUrl('/user'))
+  expect(request.headers.all()).toEqual({ 'content-type': 'application/json' })
+  expect(request.body).toEqual(JSON.stringify({ userId: 'abc-123' }))
+})
+
+it('XMLHttpRequest: emits the "request" event upon a request', async () => {
+  await createXMLHttpRequest((req) => {
+    req.open('POST', server.http.makeUrl('/user'))
+    req.setRequestHeader('Content-Type', 'application/json')
+    req.send(JSON.stringify({ userId: 'abc-123' }))
+  })
+  const [request] = requests
+
+  /**
+   * @note In Node.js XMLHttpRequest if often polyfilled by ClientRequest.
+   * This results in both XMLHttpRequest and ClientRequest interceptors
+   * emitting the "request" event.
+   */
+  expect(requests).toHaveLength(4)
+  expect(request.method).toEqual('POST')
+  expect(request.url.toString()).toEqual(server.http.makeUrl('/user'))
+  expect(request.headers.all()).toEqual({ 'content-type': 'application/json' })
+  expect(request.body).toEqual(JSON.stringify({ userId: 'abc-123' }))
+})
