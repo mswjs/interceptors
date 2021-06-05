ce5c6cd1cb8ebf2a197800e75df1329a91aa7323
Artem Zakharchenko <kettanaito@gmail.com>
2021-06-05 13:11:32 +0200

    Polyfills "setImmediate" to use in playwright

--- test/jest.browser.setup.ts
+++ test/jest.browser.setup.ts
@@ -2,8 +2,16 @@ import { createBrowser, CreateBrowserApi } from 'page-with'
 import webpackConfig from './webpack.config'
 
 let browser: CreateBrowserApi
+const originalSetImmediate = global.setImmediate
 
 beforeAll(async () => {
+  // @ts-ignore
+  // Jest@27 removes `setImmediate` without any guidance as to what to do
+  // with the third-party code that still uses it.
+  global.setImmediate = (cb) => {
+    setTimeout(cb, 0)
+  }
+
   browser = await createBrowser({
     launchOptions: {
       args: ['--allow-insecure-localhost'],
@@ -16,4 +24,5 @@ beforeAll(async () => {
 
 afterAll(async () => {
   await browser.cleanup()
+  global.setImmediate = originalSetImmediate
 })
