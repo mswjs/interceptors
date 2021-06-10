f2d2ca97e38bf397738dc7ad077263861da8f235
Artem Zakharchenko <kettanaito@gmail.com>
2021-06-11 00:10:37 +0200

    Fixes browser tests with "setImmediate"

--- test/jest.browser.setup.ts
+++ test/jest.browser.setup.ts
@@ -2,16 +2,8 @@ import { createBrowser, CreateBrowserApi } from 'page-with'
 import webpackConfig from './webpack.config'
 
 let browser: CreateBrowserApi
-const originalSetImmediate = global.setImmediate
 
 beforeAll(async () => {
-  // @ts-ignore
-  // Jest@27 removes `setImmediate` without any guidance as to what to do
-  // with the third-party code that still uses it.
-  global.setImmediate = (cb) => {
-    setTimeout(cb, 0)
-  }
-
   browser = await createBrowser({
     launchOptions: {
       args: ['--allow-insecure-localhost'],
@@ -24,5 +16,4 @@ beforeAll(async () => {
 
 afterAll(async () => {
   await browser.cleanup()
-  global.setImmediate = originalSetImmediate
 })
