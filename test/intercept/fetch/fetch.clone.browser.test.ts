2d095f4393b2e73b4edd3ce04296347c89b0bef7
Artem Zakharchenko <kettanaito@gmail.com>
2021-11-09 12:49:08 +0100

    deps: remove "chai"

--- test/intercept/fetch/fetch.clone.browser.test.ts
+++ test/intercept/fetch/fetch.clone.browser.test.ts
@@ -1,13 +1,9 @@
 import * as path from 'path'
 import { pageWith } from 'page-with'
 import { ServerApi, createServer } from '@open-draft/test-server'
-import { RequestOptions } from 'https'
 
 declare namespace window {
-  export const fetchData: (
-    url: RequestInfo,
-    expectedBody: string
-  ) => Promise<void>
+  export const fetchData: (url: RequestInfo) => Promise<void>
 }
 
 let httpServer: ServerApi
@@ -29,7 +25,20 @@ it('does not lock the original response', async () => {
     example: path.resolve(__dirname, 'fetch.clone.runtime.js'),
   })
 
-  await runtime.page.evaluate((url) => {
-    return window.fetchData(url, 'hello world')
+  runtime.page.evaluate((url) => {
+    return window.fetchData(url)
   }, httpServer.http.makeUrl('/'))
+
+  const responseText = await runtime.page.evaluate(() => {
+    return new Promise((resolve) => {
+      document.addEventListener(
+        'response-text' as any,
+        (event: CustomEvent<string>) => {
+          resolve(event.detail)
+        }
+      )
+    })
+  })
+
+  expect(responseText).toEqual('hello world')
 })
