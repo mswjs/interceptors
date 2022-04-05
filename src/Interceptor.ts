100e256545e97f4ade626a120f04c733e676266b
Artem Zakharchenko <kettanaito@gmail.com>
2022-04-05 12:52:40 +0200

    chore: add "readyState" to Interceptor and AsyncEmitter

--- src/Interceptor.ts
+++ src/Interceptor.ts
@@ -1,10 +1,11 @@
 import { Debugger, debug } from 'debug'
 import { AsyncEventEmitter } from './utils/AsyncEventEmitter'
+import { nextTick } from './utils/nextTick'
 
 export type InterceptorEventMap = Record<string, (...args: any[]) => void>
 export type InterceptorSubscription = () => void
 
-function getGlobalSymbol<V>(symbol: Symbol): V | undefined {
+export function getGlobalSymbol<V>(symbol: Symbol): V | undefined {
   return (
     // @ts-ignore https://github.com/Microsoft/TypeScript/issues/24587
     globalThis[symbol] || undefined
@@ -16,9 +17,17 @@ function setGlobalSymbol(symbol: Symbol, value: any): void {
   globalThis[symbol] = value
 }
 
-function deleteGlobalSymbol(symbol: Symbol): void {
+export function deleteGlobalSymbol(symbol: Symbol): void {
   // @ts-ignore
   delete globalThis[symbol]
+}
+
+export enum InterceptorReadyState {
+  IDLE = 'IDLE',
+  APPLYING = 'APPLYING',
+  APPLIED = 'APPLIED',
+  DISPOSING = 'DISPOSING',
+  DISPOSED = 'DISPOSED',
 }
 
 export class Interceptor<EventMap extends InterceptorEventMap> {
@@ -26,7 +35,11 @@ export class Interceptor<EventMap extends InterceptorEventMap> {
   protected subscriptions: InterceptorSubscription[]
   protected log: Debugger
 
+  public readyState: InterceptorReadyState
+
   constructor(private readonly symbol: Symbol) {
+    this.readyState = InterceptorReadyState.IDLE
+
     this.emitter = new AsyncEventEmitter()
     this.subscriptions = []
     this.log = debug(symbol.description!)
@@ -54,12 +67,25 @@ export class Interceptor<EventMap extends InterceptorEventMap> {
     const log = this.log.extend('apply')
     log('applying the interceptor...')
 
+    if (this.readyState === InterceptorReadyState.APPLIED) {
+      log('intercepted already applied!')
+      return
+    }
+
     const shouldApply = this.checkEnvironment()
 
     if (!shouldApply) {
       log('the interceptor cannot be applied in this environment!')
       return
     }
+
+    this.readyState = InterceptorReadyState.APPLYING
+
+    // Always activate the emitter when applying the interceptor.
+    // This will ensure the interceptor can process events after it's
+    // been disposed and re-applied again (it may be a singleton).
+    this.emitter.activate()
+    log('activated the emiter!', this.emitter.readyState)
 
     // Whenever applying a new interceptor, check if it hasn't been applied already.
     // This enables to apply the same interceptor multiple times, for example from a different
@@ -71,19 +97,23 @@ export class Interceptor<EventMap extends InterceptorEventMap> {
 
       // Proxy any listeners you set on this instance to the running instance.
       this.on = (event, listener) => {
-        log('proxying the "%s" listener')
+        log('proxying the "%s" listener', event)
 
         // Add listeners to the running instance so they appear
         // at the top of the event listeners list and are executed first.
         runningInstance.emitter.addListener(event, listener)
 
+        // Ensure that once this interceptor instance is disposed,
+        // it removes all listeners it has appended to the running interceptor instance.
         this.subscriptions.push(() => {
-          // Automatically remove the listener when the instance is disposed.
           runningInstance.emitter.removeListener(event, listener)
-
           log('removed proxied "%s" listener!', event)
         })
       }
+
+      nextTick(() => {
+        this.readyState = InterceptorReadyState.APPLIED
+      })
 
       return
     }
@@ -95,6 +125,10 @@ export class Interceptor<EventMap extends InterceptorEventMap> {
 
     // Store the newly applied interceptor instance globally.
     this.setInstance()
+
+    nextTick(() => {
+      this.readyState = InterceptorReadyState.APPLIED
+    })
   }
 
   /**
@@ -112,6 +146,15 @@ export class Interceptor<EventMap extends InterceptorEventMap> {
     listener: EventMap[Event]
   ): void {
     const log = this.log.extend('on')
+
+    if (
+      this.readyState === InterceptorReadyState.DISPOSING ||
+      this.readyState === InterceptorReadyState.DISPOSED
+    ) {
+      log('cannot listen to events, already disposed!')
+      return
+    }
+
     log('adding "%s" event listener:', event, listener.name)
 
     this.emitter.on(event, listener)
@@ -122,7 +165,14 @@ export class Interceptor<EventMap extends InterceptorEventMap> {
    */
   public dispose(): void {
     const log = this.log.extend('dispose')
+
+    if (this.readyState === InterceptorReadyState.DISPOSED) {
+      log('cannot dispose, already disposed!')
+      return
+    }
+
     log('disposing the interceptor...')
+    this.readyState = InterceptorReadyState.DISPOSING
 
     if (!this.getInstance()) {
       log('no interceptors running, skipping dispose...')
@@ -141,24 +191,33 @@ export class Interceptor<EventMap extends InterceptorEventMap> {
       for (const dispose of this.subscriptions) {
         dispose()
       }
+
       this.subscriptions = []
 
       log('disposed of all subscriptions!', this.subscriptions.length)
     }
 
-    this.emitter.removeAllListeners()
-    log('removed all listeners!')
+    this.emitter.destroy()
+    log('destroyed the listener!')
+
+    nextTick(() => {
+      this.readyState = InterceptorReadyState.DISPOSED
+    })
   }
 
   private getInstance(): this | undefined {
-    return getGlobalSymbol<this>(this.symbol)
+    const instance = getGlobalSymbol<this>(this.symbol)
+    this.log('retrieved global instance:', instance?.constructor?.name)
+    return instance
   }
 
   private setInstance(): void {
     setGlobalSymbol(this.symbol, this)
+    this.log('set global instance!', this.symbol.description)
   }
 
   private clearInstance(): void {
     deleteGlobalSymbol(this.symbol)
+    this.log('cleared global instance!', this.symbol.description)
   }
 }
--- src/utils/AsyncEventEmitter.ts
+++ src/utils/AsyncEventEmitter.ts
@@ -1,29 +1,63 @@
+import { Debugger, debug } from 'debug'
 import { StrictEventEmitter, EventMapType } from 'strict-event-emitter'
 import { nextTick } from './nextTick'
+
+export type QueueItem = Promise<void>
+
+export enum AsyncEventEmitterReadyState {
+  ACTIVE = 'ACTIVE',
+  DESTROYED = 'DESTROYED',
+}
 
 export class AsyncEventEmitter<
   EventMap extends EventMapType
 > extends StrictEventEmitter<EventMap> {
-  private queue: Map<keyof EventMap, Promise<void>[]> = new Map()
+  public readyState: AsyncEventEmitterReadyState
+
+  private log: Debugger
+  protected queue: Map<keyof EventMap, QueueItem[]>
+
+  constructor() {
+    super()
+
+    this.log = debug('async-event-emitter')
+    this.queue = new Map()
+
+    this.readyState = AsyncEventEmitterReadyState.ACTIVE
+  }
 
   public on<Event extends keyof EventMap>(
     event: Event,
     listener: EventMap[Event]
   ) {
+    const log = this.log.extend('on')
+
+    log('adding "%s" listener...', event)
+
+    if (this.readyState === AsyncEventEmitterReadyState.DESTROYED) {
+      log('the emitter is destroyed, skipping!')
+      return this
+    }
+
     return super.on(event, (async (...args: unknown[]) => {
       // Event queue is always established when calling ".emit()".
-      const queue = this.queue.get(event)!
+      const queue = this.openListenerQueue(event)
+
+      log('awaiting the "%s" listener...', event)
 
       // Whenever a listener is called, create a new Promise
       // that resolves when that listener function completes its execution.
       queue.push(
-        new Promise(async (resolve, reject) => {
+        new Promise<void>(async (resolve, reject) => {
           try {
             // Treat listeners as potentially asynchronous functions
             // so they could be awaited.
             await listener(...args)
             resolve()
+
+            log('"%s" listener has resolved!', event)
           } catch (error) {
+            log('"%s" listener has rejected!', error)
             reject(error)
           }
         })
@@ -35,8 +69,19 @@ export class AsyncEventEmitter<
     event: Event,
     ...args: Parameters<EventMap[Event]>
   ): boolean {
+    const log = this.log.extend('emit')
+
+    log('emitting "%s" event...', event)
+
+    if (this.readyState === AsyncEventEmitterReadyState.DESTROYED) {
+      log('the emitter is destroyed, skipping!')
+      return false
+    }
+
     // Establish the Promise queue for this particular event.
-    this.queue.set(event, [Promise.resolve()])
+    this.openListenerQueue(event)
+
+    log('appending a one-time cleanup "%s" listener...', event)
 
     // Append a one-time clean up listener.
     this.once(event, (() => {
@@ -45,6 +90,7 @@ export class AsyncEventEmitter<
       // time to properly resolve.
       nextTick(() => {
         this.queue.delete(event)
+        log('cleaned up "%s" listeners queue!', event)
       })
     }) as EventMap[Event])
 
@@ -53,12 +99,72 @@ export class AsyncEventEmitter<
 
   /**
    * Returns a promise that resolves when all the listeners for the given event
-   * has been called. Does not await the listeners themselves.
+   * has been called. Awaits asynchronous listeners.
    * If the event has no listeners, resolves immediately.
    */
   public async untilIdle<Event extends keyof EventMap>(
     event: Event
   ): Promise<void> {
-    await Promise.all(this.queue.get(event) || [])
+    const listenersQueue = this.queue.get(event) || []
+    await Promise.all(listenersQueue).finally(() => {
+      // Clear the queue one the promise settles
+      // so that different events don't share the same queue.
+      this.queue.delete(event)
+    })
+  }
+
+  private openListenerQueue<Event extends keyof EventMap>(
+    event: Event
+  ): QueueItem[] {
+    const log = this.log.extend('openListenerQueue')
+
+    log('opening "%s" listeners queue...', event)
+
+    const queue = this.queue.get(event)
+
+    if (!queue) {
+      log('no queue found, creating one...')
+
+      this.queue.set(event, [])
+      return []
+    }
+
+    log('returning an exising queue:', queue)
+    return queue
+  }
+
+  public removeAllListeners<Event extends keyof EventMap>(event?: Event) {
+    const log = this.log.extend('removeAllListeners')
+    log('event:', event)
+
+    if (event) {
+      this.queue.delete(event)
+      log('cleared the "%s" listeners queue!', event, this.queue.get(event))
+    } else {
+      this.queue.clear()
+      log('cleared the listeners queue!', this.queue)
+    }
+
+    return super.removeAllListeners(event)
+  }
+
+  public activate(): void {
+    const log = this.log.extend('activate')
+    this.readyState = AsyncEventEmitterReadyState.ACTIVE
+    log('set state to:', this.readyState)
+  }
+
+  /**
+   * Destroy this event emitter.
+   * Destroyed emitter can no longer emit and listen to events.
+   */
+  public destroy(): void {
+    const log = this.log.extend('destroy')
+
+    log('removing all listeners...')
+    this.removeAllListeners()
+
+    this.readyState = AsyncEventEmitterReadyState.DESTROYED
+    log('set state to:', this.readyState)
   }
 }
