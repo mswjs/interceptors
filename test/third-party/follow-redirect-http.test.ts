7751f3f2f8d6a7fff498d0310e20fd8620807298
Artem Zakharchenko <kettanaito@gmail.com>
2022-06-09 13:44:20 +0200

    test(follow-redirects): assert the actual redirect happening

--- test/third-party/follow-redirect-http.test.ts
+++ test/third-party/follow-redirect-http.test.ts
@@ -13,7 +13,11 @@ const interceptor = new ClientRequestInterceptor()
 interceptor.on('request', resolver)
 
 const server = new HttpServer((app) => {
-  app.post('/resource', (req, res) => {
+  app.get('/resource', (req, res) => {
+    res.status(301).set('Location', server.https.url('/user')).end()
+  })
+
+  app.get('/user', (req, res) => {
     res.status(200).send('hello from the server')
   })
 })
@@ -36,33 +40,50 @@ test('intercepts a POST request issued by "follow-redirects"', async () => {
   const { address } = server.https
   const payload = JSON.stringify({ todo: 'Buy the milk' })
 
-  const req = https.request({
-    hostname: address.host,
-    port: address.port,
-    path: '/resource',
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-      'Content-Length': payload.length,
+  const catchResponseUrl = jest.fn()
+  const req = https.request(
+    {
+      method: 'GET',
+      hostname: address.host,
+      port: address.port,
+      path: '/resource',
+      headers: {
+        'Content-Type': 'application/json',
+        'Content-Length': payload.length,
+      },
+      agent: httpsAgent,
     },
-    agent: httpsAgent,
-  })
+    (res) => {
+      catchResponseUrl(res.responseUrl)
+    }
+  )
 
   req.end(payload)
 
-  const { text } = await waitForClientRequest(req as any)
-  expect(resolver).toHaveBeenCalledTimes(1)
+  const { res, text } = await waitForClientRequest(req as any)
+  expect(resolver).toHaveBeenCalledTimes(2)
 
-  // Intercepted request.
-  const request = resolver.mock.calls[0][0]
+  // Intercepted initial request.
+  const [initialRequest] = resolver.mock.calls[0]
 
-  expect(request.method).toBe('POST')
-  expect(request.url.href).toBe(server.https.url('/resource'))
-  expect(request.credentials).toBe('same-origin')
-  expect(request.headers.get('Content-Type')).toBe('application/json')
-  expect(request.headers.get('Content-Length')).toBe('23')
-  expect(request.body).toBe(JSON.stringify({ todo: 'Buy the milk' }))
+  expect(initialRequest.method).toBe('GET')
+  expect(initialRequest.url.href).toBe(server.https.url('/resource'))
+  expect(initialRequest.credentials).toBe('same-origin')
+  expect(initialRequest.headers.get('Content-Type')).toBe('application/json')
+  expect(initialRequest.headers.get('Content-Length')).toBe('23')
+  expect(initialRequest.body).toBe(JSON.stringify({ todo: 'Buy the milk' }))
+
+  // Intercepted redirect request (issed by "follow-redirects").
+  const [redirectedRequest] = resolver.mock.calls[1]
+
+  expect(redirectedRequest.method).toBe('GET')
+  expect(redirectedRequest.url.href).toBe(server.https.url('/user'))
+  expect(redirectedRequest.credentials).toBe('same-origin')
+  expect(redirectedRequest.headers.get('Content-Type')).toBe('application/json')
+  expect(redirectedRequest.headers.get('Content-Length')).toBe('23')
+  expect(redirectedRequest.body).toBe(JSON.stringify({ todo: 'Buy the milk' }))
 
   // Response (original).
+  expect(catchResponseUrl).toHaveBeenCalledWith(server.https.url('/user'))
   expect(await text()).toBe('hello from the server')
 })
