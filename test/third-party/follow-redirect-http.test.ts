2c64bdbe236051c962d22357806876e6e23cffff
Artem Zakharchenko <kettanaito@gmail.com>
2022-06-09 13:28:28 +0200

    test(follow-redirects): await response, use "test-server"

--- test/third-party/follow-redirect-http.test.ts
+++ test/third-party/follow-redirect-http.test.ts
@@ -1,66 +1,68 @@
 /**
  * @jest-environment jsdom
  */
-import express from 'express'
 import { https } from 'follow-redirects'
+import { httpsAgent, HttpServer } from '@open-draft/test-server/http'
 import { ClientRequestInterceptor } from '../../src/interceptors/ClientRequest'
-import { IsomorphicRequest } from '../../src/glossary'
+import type { HttpRequestEventMap } from '../../src/glossary'
+import { waitForClientRequest } from '../helpers'
 
-let requests: IsomorphicRequest[] = []
+const resolver = jest.fn<never, Parameters<HttpRequestEventMap['request']>>()
 
 const interceptor = new ClientRequestInterceptor()
-interceptor.on('request', (request) => {
-  requests.push(request)
+interceptor.on('request', resolver)
+
+const server = new HttpServer((app) => {
+  app.post('/resource', (req, res) => {
+    res.status(200).send('hello from the server')
+  })
 })
 
-const app = express()
-app.use(express.json())
-app.post('/', (req, res) => {
-  res.status(200).json(req.body)
-})
-
-beforeAll(() => {
+beforeAll(async () => {
+  await server.listen()
   interceptor.apply()
 })
 
 afterEach(() => {
-  requests = []
+  jest.resetAllMocks()
 })
 
-afterAll(() => {
+afterAll(async () => {
   interceptor.dispose()
+  await server.close()
 })
 
-test('request is sent with req.end(data)', async () => {
-  const data = JSON.stringify({ todo: 'Buy the milk' })
+test('intercepts a POST request issued by "follow-redirects"', async () => {
+  const { address } = server.https
+  const payload = JSON.stringify({ todo: 'Buy the milk' })
 
-  const options = {
-    hostname: 'localhost',
-    port: 443,
-    path: '/',
+  const req = https.request({
+    hostname: address.host,
+    port: address.port,
+    path: '/resource',
     method: 'POST',
     headers: {
       'Content-Type': 'application/json',
-      'Content-Length': data.length,
+      'Content-Length': payload.length,
     },
-  }
-
-  const req = https.request(options, (res) => {
-    console.log(`statusCode: ${res.statusCode}`)
-
-    res.on('data', (d) => {
-      process.stdout.write(d)
-    })
+    agent: httpsAgent,
   })
 
-  req.on('error', (error) => {
-    console.error(error)
-  })
+  req.end(payload)
 
-  req.end(data)
+  const { text } = await waitForClientRequest(req as any)
+  expect(resolver).toHaveBeenCalledTimes(1)
 
-  expect(requests).toHaveLength(1)
-  const [request] = requests
+  // Intercepted request.
+  const request = resolver.mock.calls[0][0]
+
   expect(request.method).toBe('POST')
-  expect(request.body).toEqual(JSON.stringify({ todo: 'Buy the milk' }))
+  expect(request.url.href).toBe(server.https.url('/resource'))
+  expect(request.credentials).toBe('same-origin')
+  expect(request.headers.get('Content-Type')).toBe('application/json')
+  expect(request.headers.get('Content-Length')).toBe('23')
+  expect(request.body).toBe(JSON.stringify({ todo: 'Buy the milk' }))
+
+  // Response (original).
+  expect(await text()).toBe('hello from the server')
 })
