30f51f8e9e9793d3283341f7779ef6f6fdeffcbd
Gurwinder Singh <vargwin@gmail.com>
2022-07-03 21:11:22 +0530

    test: isomorhpic request copy constructor test

--- src/IsomorphicRequest.test.ts
+++ src/IsomorphicRequest.test.ts
@@ -2,7 +2,7 @@ import { Headers } from 'headers-polyfill/lib'
 import { IsomorphicRequest } from './IsomorphicRequest'
 import { encodeBuffer } from './utils/bufferUtils'
 
-describe('Request', () => {
+describe('IsomorphicRequest', () => {
   const url = new URL('http://dummy')
   const body = encodeBuffer(JSON.stringify({ hello: 'world' }))
 
@@ -48,7 +48,7 @@ describe('Request', () => {
     expect(request.headers).toEqual(new Headers())
   })
 
-  it('return empty headers if not provided', () => {
+  it('return given headers', () => {
     const request = new IsomorphicRequest(url, {
       body,
       headers: { 'Content-Type': 'application/json' },
@@ -56,5 +56,19 @@ describe('Request', () => {
     expect(request.headers).toEqual(
       new Headers({ 'Content-Type': 'application/json' })
     )
+  })
+
+  it('returns a copy of isomorphic request instance', () => {
+    const request = new IsomorphicRequest(url, {
+      body,
+      headers: { foo: 'bar' },
+    })
+    const request2 = new IsomorphicRequest(request)
+    expect(request.id).toEqual(request2.id)
+    expect(request.url).toEqual(request2.url)
+    expect(request.body).toEqual(request2.body)
+    expect(request.headers).toEqual(request2.headers)
+    expect(request.method).toEqual(request2.method)
+    expect(request.credentials).toEqual(request2.credentials)
   })
 })
