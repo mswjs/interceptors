bf5716ad39e48994364770bd164cfcbb2e878479
Henning Pohlmeyer <hpohlmeyer@users.noreply.github.com>
2022-09-09 10:22:04 +0200

    chore: remove custom event from test runtime

--- test/modules/send-beacon/intercept/send-beacon.browser.runtime.js
+++ test/modules/send-beacon/intercept/send-beacon.browser.runtime.js
@@ -1,22 +1,5 @@
 import { SendBeaconInterceptor } from '@mswjs/interceptors/lib/interceptors/sendBeacon'
 
-// Dispatch a `pure-beacon` event before calling `sendBeacon`
-// to make sure we are calling the original sendBeacon.
-// This needs to be done before we apply the interceptor
-// to ensure it will be used instead of the original version.
-const pureSendBeacon = navigator.sendBeacon
-navigator.sendBeacon = (url, data) => {
-  window.dispatchEvent(
-    new CustomEvent('pure-beacon', {
-      detail: {
-        url,
-        data,
-      },
-    })
-  )
-  pureSendBeacon(url, data)
-}
-
 const interceptor = new SendBeaconInterceptor()
 interceptor.on('request', async (request) => {
   window.dispatchEvent(
