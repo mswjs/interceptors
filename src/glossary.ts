d36aa5eac1669bb62f5be2e7fdbb8bab37978754
Artem Zakharchenko <kettanaito@gmail.com>
2022-10-12 18:21:48 +0200

    fix: remove anything isomorphic response

--- src/glossary.ts
+++ src/glossary.ts
@@ -1,22 +1,9 @@
-import type { HeadersObject, Headers } from 'headers-polyfill'
 import type { InteractiveIsomorphicRequest } from './InteractiveIsomorphicRequest'
 import type { IsomorphicRequest } from './IsomorphicRequest'
 
 export const IS_PATCHED_MODULE: unique symbol = Symbol('isPatchedModule')
 
 export type RequestCredentials = 'omit' | 'include' | 'same-origin'
-
-export interface IsomorphicResponse {
-  status: number
-  statusText: string
-  headers: Headers
-  body?: string
-}
-
-export interface MockedResponse
-  extends Omit<Partial<IsomorphicResponse>, 'headers'> {
-  headers?: HeadersObject
-}
 
 export type HttpRequestEventMap = {
   request(request: InteractiveIsomorphicRequest): Promise<void> | void
