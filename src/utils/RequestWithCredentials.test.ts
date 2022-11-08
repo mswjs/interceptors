7af9af7126416dc281c16a7f08cfa1985cb99c13
Artem Zakharchenko <kettanaito@gmail.com>
2022-11-08 01:47:37 +0100

    test: add instance test to "RequestWithCredentials"

--- src/utils/RequestWithCredentials.test.ts
+++ src/utils/RequestWithCredentials.test.ts
@@ -1,4 +1,11 @@
+import { Request } from '@remix-run/web-fetch'
 import { RequestWithCredentials } from './RequestWithCredentials'
+
+it('remains an instance of "Request"', () => {
+  const request = new RequestWithCredentials('https://example.com')
+
+  expect(request).toBeInstanceOf(Request)
+})
 
 it('sets "credentials" to "same-origin" by default', () => {
   const request = new RequestWithCredentials('https://example.com')
