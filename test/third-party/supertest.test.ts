8e6c64bd08c4f4b1ffbc8de437c51a07354f3edb
Artem Zakharchenko <kettanaito@gmail.com>
2022-11-19 02:07:49 +0100

    test(supertest): add GET request interception test

--- test/third-party/supertest.test.ts
+++ test/third-party/supertest.test.ts
@@ -22,7 +22,10 @@ interceptor.on('response', responseListener)
 const app = express()
 
 app.use(express.json())
-app.post('/', (req, res) => {
+app.get('/resource', (_, res) => {
+  res.status(200).set('Content-Type', 'text/plain').send('get-request-body')
+})
+app.post('/resource', (req, res) => {
   res.status(200).json(req.body)
 })
 
@@ -38,9 +41,33 @@ afterAll(() => {
   interceptor.dispose()
 })
 
-test('preserves original POST request JSON body', async () => {
+test('intercepts a GET request', async () => {
+  const response = await supertest(app).get('/resource')
+
+  expect(response.error).toBeFalsy()
+  expect(response.status).toBe(200)
+  expect(response.text).toBe('get-request-body')
+
+  // Must call the "request" listener.
+  expect(requestListener).toHaveBeenCalledTimes(1)
+  const [request] = requestListener.mock.calls[0]
+  expect(request.method).toBe('GET')
+  expect(request.body).toBeNull()
+
+  // Must call the "response" listener.
+  expect(responseListener).toHaveBeenCalledTimes(1)
+  const [responseFromListener] = responseListener.mock.calls[0]
+  expect(responseFromListener.status).toBe(200)
+  expect(responseFromListener.statusText).toBe('OK')
+  expect(responseFromListener.headers.get('content-type')).toBe(
+    'text/plain; charset=utf-8'
+  )
+  expect(await responseFromListener.text()).toBe('get-request-body')
+})
+
+test('intercepts a POST request', async () => {
   const response = await supertest(app)
-    .post('/')
+    .post('/resource')
     .set('Content-Type', 'application/json')
     .send({ query: 'foo' })
 
