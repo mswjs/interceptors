ddf0389c0a0f2f98350f3857d1e301f806c66587
Artem Zakharchenko <kettanaito@gmail.com>
2022-11-19 02:10:44 +0100

    test(supertest): add event listener tests (#305)
    * test(supertest): add event listener tests
    
    * test(supertest): add GET request interception test

--- test/third-party/supertest.test.ts
+++ test/third-party/supertest.test.ts
@@ -3,18 +3,29 @@
  */
 import express from 'express'
 import supertest from 'supertest'
+import { HttpRequestEventMap } from '../../src'
 import { ClientRequestInterceptor } from '../../src/interceptors/ClientRequest'
 
-let requests: Array<Request> = []
+const requestListener = jest.fn<
+  ReturnType<HttpRequestEventMap['request']>,
+  Parameters<HttpRequestEventMap['request']>
+>()
+const responseListener = jest.fn<
+  ReturnType<HttpRequestEventMap['response']>,
+  Parameters<HttpRequestEventMap['response']>
+>()
 
 const interceptor = new ClientRequestInterceptor()
-interceptor.on('request', (request) => {
-  requests.push(request)
-})
+interceptor.on('request', requestListener)
+interceptor.on('response', responseListener)
 
 const app = express()
+
 app.use(express.json())
-app.post('/', (req, res) => {
+app.get('/resource', (_, res) => {
+  res.status(200).set('Content-Type', 'text/plain').send('get-request-body')
+})
+app.post('/resource', (req, res) => {
   res.status(200).json(req.body)
 })
 
@@ -23,25 +34,60 @@ beforeAll(() => {
 })
 
 afterEach(() => {
-  requests = []
+  jest.resetAllMocks()
 })
 
 afterAll(() => {
   interceptor.dispose()
 })
 
-test('preserves original POST request JSON body', async () => {
-  const res = await supertest(app)
-    .post('/')
+test('intercepts a GET request', async () => {
+  const response = await supertest(app).get('/resource')
+
+  expect(response.error).toBeFalsy()
+  expect(response.status).toBe(200)
+  expect(response.text).toBe('get-request-body')
+
+  // Must call the "request" listener.
+  expect(requestListener).toHaveBeenCalledTimes(1)
+  const [request] = requestListener.mock.calls[0]
+  expect(request.method).toBe('GET')
+  expect(request.body).toBeNull()
+
+  // Must call the "response" listener.
+  expect(responseListener).toHaveBeenCalledTimes(1)
+  const [responseFromListener] = responseListener.mock.calls[0]
+  expect(responseFromListener.status).toBe(200)
+  expect(responseFromListener.statusText).toBe('OK')
+  expect(responseFromListener.headers.get('content-type')).toBe(
+    'text/plain; charset=utf-8'
+  )
+  expect(await responseFromListener.text()).toBe('get-request-body')
+})
+
+test('intercepts a POST request', async () => {
+  const response = await supertest(app)
+    .post('/resource')
     .set('Content-Type', 'application/json')
     .send({ query: 'foo' })
 
-  expect(res.error).toBeFalsy()
-  expect(res.status).toBe(200)
-  expect(res.body).toEqual({ query: 'foo' })
+  expect(response.error).toBeFalsy()
+  expect(response.status).toBe(200)
+  expect(response.body).toEqual({ query: 'foo' })
 
-  expect(requests).toHaveLength(1)
-  const [request] = requests
+  // Must call the "request" listener.
+  expect(requestListener).toHaveBeenCalledTimes(1)
+  const [request] = requestListener.mock.calls[0]
   expect(request.method).toBe('POST')
   expect(await request.json()).toEqual({ query: 'foo' })
+
+  // Must call the "response" listener.
+  expect(responseListener).toHaveBeenCalledTimes(1)
+  const [responseFromListener] = responseListener.mock.calls[0]
+  expect(responseFromListener.status).toBe(200)
+  expect(responseFromListener.statusText).toBe('OK')
+  expect(responseFromListener.headers.get('content-type')).toBe(
+    'application/json; charset=utf-8'
+  )
+  expect(await responseFromListener.json()).toEqual({ query: 'foo' })
 })
