b0b291161127ed88897a88e74c167fd866501b6a
Jorge Israel Peña <jorge.israel.p@gmail.com>
2022-12-13 17:07:40 -0800

    Finalize response on 'load'

--- src/interceptors/XMLHttpRequest/XMLHttpRequestOverride.ts
+++ src/interceptors/XMLHttpRequest/XMLHttpRequestOverride.ts
@@ -399,8 +399,6 @@ export const createXMLHttpRequestOverride = (
             this.password
           )
 
-          let finalized = false;
-
           originalRequest.addEventListener('readystatechange', () => {
             // Forward the original response headers to the patched instance
             // immediately as they are received.
@@ -415,25 +413,6 @@ export const createXMLHttpRequestOverride = (
                 'original response headers (normalized)',
                 this._responseHeaders
               )
-            } else if (originalRequest.readyState === XMLHttpRequest.DONE) {
-              if (finalized) {
-                return;
-              }
-
-              finalized = true;
-
-              this.status = originalRequest.status
-              this.statusText = originalRequest.statusText
-              this.responseURL = originalRequest.responseURL
-              this.log('received original response', this.status, this.statusText)
-
-              // Explicitly mark the mocked request instance as done
-              // so the response never hangs.
-              this.setReadyState(this.DONE)
-              this.log('set mock request readyState to DONE')
-
-              this.log('original response body:', this.response)
-              this.log('original response finished!')
             }
           })
 
@@ -449,6 +428,21 @@ export const createXMLHttpRequestOverride = (
               encodeBuffer(originalRequest.responseText)
             )
           })
+
+          originalRequest.addEventListener('load', () => {
+            this.status = originalRequest.status
+            this.statusText = originalRequest.statusText
+            this.responseURL = originalRequest.responseURL
+            this.log('received original response', this.status, this.statusText)
+
+            // Explicitly mark the mocked request instance as done
+            // so the response never hangs.
+            this.setReadyState(this.DONE)
+            this.log('set mock request readyState to DONE')
+
+            this.log('original response body:', this.response)
+            this.log('original response finished!')
+          });
 
           // Update the patched instance on the "loadend" event
           // because it fires when the request settles (succeeds/errors).
