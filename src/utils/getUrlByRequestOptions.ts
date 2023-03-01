7b46cec159028d23791e55f4233df684d1676a05
Artem Zakharchenko <kettanaito@gmail.com>
2023-03-01 18:51:48 +0100

    fix: improve node.js host resolution (#354)
    - Fixes #353 
    
    This should also make the URL resolution a bit faster in the case when
    `options.uri` is explicitly provided because we won't attempt to figure
    out URL parts from the options then.

--- src/utils/getUrlByRequestOptions.ts
+++ src/utils/getUrlByRequestOptions.ts
@@ -15,7 +15,6 @@ export type ResolvedRequestOptions = RequestOptions & RequestSelf
 export const DEFAULT_PATH = '/'
 const DEFAULT_PROTOCOL = 'http:'
 const DEFAULT_HOST = 'localhost'
-const DEFAULT_PORT = 80
 const SSL_PORT = 443
 
 function getAgent(
@@ -45,20 +44,45 @@ function getProtocolByRequestOptions(options: ResolvedRequestOptions): string {
 function getPortByRequestOptions(
   options: ResolvedRequestOptions
 ): number | undefined {
-  const agent = getAgent(options)
-  const agentPort =
-    (agent as HttpsAgent)?.options.port ||
-    (agent as RequestOptions)?.defaultPort
-  const optionsPort = options.port
-
-  if (optionsPort || agentPort) {
-    const explicitPort = optionsPort || agentPort || DEFAULT_PORT
-    return Number(explicitPort)
+  // Use the explicitly provided port.
+  if (options.port) {
+    return Number(options.port)
   }
+
+  // Extract the port from the hostname.
+  if (options.hostname != null) {
+    const [, extractedPort] = options.hostname.match(/:(\d+)$/) || []
+
+    if (extractedPort != null) {
+      return Number(extractedPort)
+    }
+  }
+
+  // Otherwise, try to resolve port from the agent.
+  const agent = getAgent(options)
+
+  if ((agent as HttpsAgent)?.options.port) {
+    return Number((agent as HttpsAgent).options.port)
+  }
+
+  if ((agent as RequestOptions)?.defaultPort) {
+    return Number((agent as RequestOptions).defaultPort)
+  }
+
+  // Lastly, return undefined indicating that the port
+  // must inferred from the protocol. Do not infer it here.
+  return undefined
 }
 
 function getHostByRequestOptions(options: ResolvedRequestOptions): string {
-  return options.hostname || options.host || DEFAULT_HOST
+  const { hostname, host } = options
+
+  // If the hostname is specified, resolve the host from the "host:port" string.
+  if (hostname != null) {
+    return hostname.replace(/:\d+$/, '')
+  }
+
+  return host || DEFAULT_HOST
 }
 
 function getAuthByRequestOptions(options: ResolvedRequestOptions) {
@@ -77,45 +101,65 @@ function isRawIPv6Address(host: string): boolean {
   return host.includes(':') && !host.startsWith('[') && !host.endsWith(']')
 }
 
+function getHostname(host: string, port?: number): string {
+  const portString = typeof port !== 'undefined' ? `:${port}` : ''
+
+  /**
+   * @note As of Node >= 17, hosts (including "localhost") can resolve to IPv6
+   * addresses, so construct valid URL by surrounding the IPv6 host with brackets.
+   */
+  if (isRawIPv6Address(host)) {
+    return `[${host}]${portString}`
+  }
+
+  if (typeof port === 'undefined') {
+    return host
+  }
+
+  return `${host}${portString}`
+}
+
 /**
  * Creates a `URL` instance from a given `RequestOptions` object.
  */
 export function getUrlByRequestOptions(options: ResolvedRequestOptions): URL {
   debug('request options', options)
 
-  const protocol = getProtocolByRequestOptions(options)
-  const host = getHostByRequestOptions(options)
-  const port = getPortByRequestOptions(options)
-  const path = options.path || DEFAULT_PATH
-  const auth = getAuthByRequestOptions(options)
+  if (options.uri) {
+    debug(
+      'constructing url from explicitly provided "options.uri": %s',
+      options.uri
+    )
+    return new URL(options.uri.href)
+  }
 
+  debug('figuring out url from request options...')
+
+  const protocol = getProtocolByRequestOptions(options)
   debug('protocol', protocol)
+
+  const host = getHostByRequestOptions(options)
   debug('host', host)
+
+  const port = getPortByRequestOptions(options)
   debug('port', port)
+
+  const hostname = getHostname(host, port)
+  debug('hostname', hostname)
+
+  const path = options.path || DEFAULT_PATH
   debug('path', path)
 
-  /**
-   * @note As of Node >= 17, hosts (including "localhost") can resolve to IPv6
-   * addresses, so construct valid URL by surrounding the IPv6 host with brackets.
-   */
-  const baseUrl = `${protocol}//${isRawIPv6Address(host) ? `[${host}]` : host}`
-  debug('base URL:', baseUrl)
+  const credentials = getAuthByRequestOptions(options)
+  debug('credentials', credentials)
 
-  const url = options.uri ? new URL(options.uri.href) : new URL(path, baseUrl)
+  const authString = credentials
+    ? `${credentials.username}:${credentials.password}@`
+    : ''
+  debug('auth string:', authString)
 
-  if (port) {
-    debug('detected explicit port', port)
-    url.port = port.toString()
-  }
-
-  if (auth) {
-    debug('resolved auth', auth)
-
-    url.username = auth.username
-    url.password = auth.password
-  }
-
-  debug('created URL:', url)
+  const url = new URL(`${protocol}//${authString}${hostname}${path}`)
+  debug('created url:', url)
 
   return url
 }
