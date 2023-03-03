c608f24ab7e3ab6084053b13be23ca9b3d513276
Artem Zakharchenko <kettanaito@gmail.com>
2023-03-03 15:14:35 +0100

    test: add integration tests for presets

---
+++ test/features/presets/browser-preset.runtime.js
@@ -0,0 +1,26 @@
+import { BatchInterceptor } from '@mswjs/interceptors'
+import browserInterceptors from '@mswjs/interceptors/presets/browser'
+
+const interceptor = new BatchInterceptor({
+  name: 'browser-preset-interceptor',
+  interceptors: browserInterceptors,
+})
+
+interceptor.apply()
+
+interceptor.on('request', async (request, requestId) => {
+  window.dispatchEvent(
+    new CustomEvent('resolver', {
+      detail: {
+        id: requestId,
+        method: request.method,
+        url: request.url,
+        headers: Object.fromEntries(request.headers.entries()),
+        credentials: request.credentials,
+        body: await request.clone().text(),
+      },
+    })
+  )
+
+  request.respondWith(new Response('mocked'))
+})
