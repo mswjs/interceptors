28ef6670c17c164c3516b59936ae8f05d07e5f89
Henning Pohlmeyer <hpohlmeyer@users.noreply.github.com>
2023-06-28 13:33:57 +0200

    chore: remove describe block from tests

--- src/utils/findPropertySource.test.ts
+++ src/utils/findPropertySource.test.ts
@@ -1,29 +1,27 @@
-import { describe, it, expect } from 'vitest'
+import { it, expect } from 'vitest'
 import { findPropertySource } from './findPropertySource'
 
-describe('findPropertySource', () => {
-  it('does return the source for objects without prototypes', () => {
-    const obj = Object.create(null)
-    obj.test = undefined
-    const source = findPropertySource(obj, 'test')
-    expect(source).toBe(obj)
-  })
+it('returns the source for objects without prototypes', () => {
+  const obj = Object.create(null)
+  obj.test = undefined
+  const source = findPropertySource(obj, 'test')
+  expect(source).toBe(obj)
+})
 
-  it('does return the source for objects with prototypes', () => {
-    const prototype = Object.create(null)
-    prototype.test = undefined
+it('returns the source for objects with prototypes', () => {
+  const prototype = Object.create(null)
+  prototype.test = undefined
 
-    const obj = Object.create(prototype)
+  const obj = Object.create(prototype)
 
-    const source = findPropertySource(obj, 'test')
-    expect(source).toBe(prototype)
-  })
+  const source = findPropertySource(obj, 'test')
+  expect(source).toBe(prototype)
+})
 
-  it('does return null if the prototype chain does not contain the property', () => {
-    const prototype = Object.create(null)
-    const obj = Object.create(prototype)
+it('returns null if the prototype chain does not contain the property', () => {
+  const prototype = Object.create(null)
+  const obj = Object.create(prototype)
 
-    const source = findPropertySource(obj, 'test')
-    expect(source).toBeNull()
-  })
+  const source = findPropertySource(obj, 'test')
+  expect(source).toBeNull()
 })
