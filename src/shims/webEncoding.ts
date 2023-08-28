7df9776f72184e1e5c34f31cd8848b94dca7d570
Artem Zakharchenko <kettanaito@gmail.com>
2023-08-28 21:11:48 +0200

    chore: migrate to pnpm (#407)

--- src/shims/webEncoding.ts
+++
@@ -1,9 +0,0 @@
-export const TextEncoder: typeof globalThis.TextEncoder =
-  typeof globalThis.TextEncoder === 'undefined'
-    ? require('util').TextEncoder
-    : globalThis.TextEncoder
-
-export const TextDecoder: typeof globalThis.TextDecoder =
-  typeof globalThis.TextDecoder === 'undefined'
-    ? require('util').TextDecoder
-    : globalThis.TextDecoder
