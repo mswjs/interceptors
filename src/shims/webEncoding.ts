9402c7753086a3a98e12518a3550d00a4949d2e5
Artem Zakharchenko <kettanaito@gmail.com>
2023-08-28 20:45:51 +0200

    fix: remove "webEncoding" shims

--- src/shims/webEncoding.ts
+++
@@ -1,9 +0,0 @@
-export const TextEncoder: typeof globalThis.TextEncoder =
-  typeof globalThis.TextEncoder === 'undefined'
-    ? require('node:util').TextEncoder
-    : globalThis.TextEncoder
-
-export const TextDecoder: typeof globalThis.TextDecoder =
-  typeof globalThis.TextDecoder === 'undefined'
-    ? require('node:util').TextDecoder
-    : globalThis.TextDecoder
