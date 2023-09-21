95a5de7435bdf15e7f63633b8f72febd9382844c
Artem Zakharchenko <kettanaito@gmail.com>
2023-09-21 13:02:43 +0200

    fix(ClientRequest): ignore message body for responses without body

--- src/interceptors/ClientRequest/utils/createResponse.ts
+++ src/interceptors/ClientRequest/utils/createResponse.ts
@@ -1,24 +1,29 @@
 import type { IncomingHttpHeaders, IncomingMessage } from 'http'
+import { responseStatusCodesWithoutBody } from '../../../utils/responseUtils'
 
 /**
  * Creates a Fetch API `Response` instance from the given
  * `http.IncomingMessage` instance.
  */
 export function createResponse(message: IncomingMessage): Response {
-  const readable = new ReadableStream({
-    start(controller) {
-      message.on('data', (chunk) => controller.enqueue(chunk))
-      message.on('end', () => controller.close())
+  const responseBodyOrNull = responseStatusCodesWithoutBody.includes(
+    message.statusCode || 200
+  )
+    ? null
+    : new ReadableStream({
+        start(controller) {
+          message.on('data', (chunk) => controller.enqueue(chunk))
+          message.on('end', () => controller.close())
 
-      /**
-       * @todo Should also listen to the "error" on the message
-       * and forward it to the controller. Otherwise the stream
-       * will pend indefinitely.
-       */
-    },
-  })
+          /**
+           * @todo Should also listen to the "error" on the message
+           * and forward it to the controller. Otherwise the stream
+           * will pend indefinitely.
+           */
+        },
+      })
 
-  return new Response(readable, {
+  return new Response(responseBodyOrNull, {
     status: message.statusCode,
     statusText: message.statusMessage,
     headers: createHeadersFromIncomingHttpHeaders(message.headers),
