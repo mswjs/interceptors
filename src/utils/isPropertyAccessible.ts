a9cce46a3244301cc14b847225b5c7033bf10b1d
Matthew Costabile <mattcosta7@github.com>
2023-11-08 00:21:29 -0500

    chore: document isPropertyAccessible

--- src/utils/isPropertyAccessible.ts
+++ src/utils/isPropertyAccessible.ts
@@ -1,3 +1,11 @@
+/**
+ * A function that validates if property access is possible on an object
+ * without throwing. It returns `true` if the property access is possible
+ * and `false` otherwise.
+ *
+ * Environments like miniflare will throw on property access on certain objects
+ * like Request and Response, for unimplemented properties.
+ */
 export function isPropertyAccessible<Obj extends Record<string, any>>(
   obj: Obj,
   key: keyof Obj
