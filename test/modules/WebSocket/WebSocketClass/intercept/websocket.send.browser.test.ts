8f565819d77e94013b37bd5fcd328ebc0b3d5d52
Artem Zakharchenko <kettanaito@gmail.com>
2024-01-29 10:01:04 +0100

    test(ws): add send errors and buffering tests

--- test/modules/WebSocket/WebSocketClass/intercept/websocket.send.browser.test.ts
+++ test/modules/WebSocket/WebSocketClass/intercept/websocket.send.browser.test.ts
@@ -9,6 +9,24 @@ declare global {
   }
 }
 
+test('errors when sending data before open', async ({ loadExample, page }) => {
+  await loadExample(require.resolve('../websocket.runtime.js'))
+  await page.evaluate(() => window.interceptor.apply())
+
+  const sendError = await page.evaluate(() => {
+    const ws = new WebSocket('wss://example.com')
+    try {
+      ws.send('no-op')
+    } catch (error) {
+      if (error instanceof Error) {
+        return error.message
+      }
+    }
+  })
+
+  expect(sendError).toBe('InvalidStateError')
+})
+
 test('intercepts text sent over websocket', async ({ loadExample, page }) => {
   await loadExample(require.resolve('../websocket.runtime.js'))
 
@@ -86,4 +104,41 @@ test('intercepts ArrayBuffer sent over websocket', async ({
     return new TextDecoder().decode(window.outgoingData[0] as Uint8Array)
   })
   expect(outgoingData).toBe('hello from client')
+})
+
+test('increases "bufferedAmount" before the data is sent', async ({
+  loadExample,
+  page,
+}) => {
+  await loadExample(require.resolve('../websocket.runtime.js'))
+
+  await page.evaluate(() => {
+    const { interceptor, outgoingData } = window
+
+    interceptor.on('connection', ({ client }) => {
+      client.on('message', (data) => outgoingData.push(data))
+    })
+    interceptor.apply()
+  })
+
+  const bufferedAmount = await page.evaluate(() => {
+    const ws = new WebSocket('wss://example.com')
+
+    return new Promise<{ beforeSend: number; afterSend: number }>((resolve) => {
+      ws.addEventListener('open', () => {
+        ws.send('hello from client')
+        const beforeSend = ws.bufferedAmount
+
+        queueMicrotask(() => {
+          resolve({
+            beforeSend,
+            afterSend: ws.bufferedAmount,
+          })
+        })
+      })
+    })
+  })
+
+  expect(bufferedAmount.beforeSend).toBe(17)
+  expect(bufferedAmount.afterSend).toBe(0)
 })
