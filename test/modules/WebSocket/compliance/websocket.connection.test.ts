6bd132f1f024b9ec00bddb091d5bc04485eb7768
Artem Zakharchenko <kettanaito@gmail.com>
2024-03-12 13:35:40 +0100

    fix(WebSocket): emit "connection" event on the next tick (#518)

--- test/modules/WebSocket/compliance/websocket.connection.test.ts
+++ test/modules/WebSocket/compliance/websocket.connection.test.ts
@@ -4,6 +4,7 @@
 import { vi, it, expect, beforeAll, afterAll } from 'vitest'
 import { WebSocketInterceptor } from '../../../../src/interceptors/WebSocket/index'
 import { UUID_REGEXP } from '../../../helpers'
+import { waitForNextTick } from '../utils/waitForNextTick'
 
 const interceptor = new WebSocketInterceptor()
 
@@ -15,30 +16,35 @@ afterAll(() => {
   interceptor.dispose()
 })
 
-it('emits the correct connection event', async () => {
+it('emits the correct "connection" event on the interceptor', async () => {
   const connectionListener = vi.fn()
   interceptor.once('connection', connectionListener)
 
   new WebSocket('wss://example.com')
 
-  await vi.waitFor(() => {
-    expect(connectionListener).toHaveBeenCalledTimes(1)
-    expect(connectionListener).toHaveBeenNthCalledWith(
-      1,
-      expect.objectContaining({
-        client: expect.objectContaining({
-          id: expect.stringMatching(UUID_REGEXP),
-          send: expect.any(Function),
-          addEventListener: expect.any(Function),
-          removeEventListener: expect.any(Function),
-          close: expect.any(Function),
-        }),
-        server: expect.objectContaining({
-          send: expect.any(Function),
-          addEventListener: expect.any(Function),
-          removeEventListener: expect.any(Function),
-        }),
-      })
-    )
-  })
+  // Must not emit the "connection" event on this tick.
+  expect(connectionListener).toHaveBeenCalledTimes(0)
+
+  await waitForNextTick()
+
+  // Must emit the "connection" event on the next tick
+  // so the client can modify the WebSocket instance meanwhile.
+  expect(connectionListener).toHaveBeenCalledTimes(1)
+  expect(connectionListener).toHaveBeenNthCalledWith(
+    1,
+    expect.objectContaining({
+      client: expect.objectContaining({
+        id: expect.stringMatching(UUID_REGEXP),
+        send: expect.any(Function),
+        addEventListener: expect.any(Function),
+        removeEventListener: expect.any(Function),
+        close: expect.any(Function),
+      }),
+      server: expect.objectContaining({
+        send: expect.any(Function),
+        addEventListener: expect.any(Function),
+        removeEventListener: expect.any(Function),
+      }),
+    })
+  )
 })
