f2b7c508aceeebac522d2c5e7b2e91ba8174414a
Artem Zakharchenko <kettanaito@gmail.com>
2024-03-27 15:52:12 +0100

    fix: export "randomId()" publicly

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ export * from './Interceptor'
 export * from './BatchInterceptor'
 
 /* Utils */
+export { randomId } from './randomId'
 export { getCleanUrl } from './utils/getCleanUrl'
 export { encodeBuffer, decodeBuffer } from './utils/bufferUtils'
 export { isResponseWithoutBody } from './utils/responseUtils'
