812d590351241c8703545354edeb060496fd65de
Artem Zakharchenko <kettanaito@gmail.com>
2024-03-29 19:17:21 +0100

    fix(WebSocket): emit "close" event on "server.close()" (#538)

--- test/modules/WebSocket/compliance/websocket.server.events.test.ts
+++ test/modules/WebSocket/compliance/websocket.server.events.test.ts
@@ -54,3 +54,36 @@ it('emits "open" event if the listener was added before calling "connect()"', as
     expect(serverOpenListener).toHaveBeenCalledTimes(1)
   })
 })
+
+it('emits "close" event when the server connection is closed', async () => {
+  wsServer.addListener('connection', (ws) => {
+    queueMicrotask(() => ws.close())
+  })
+
+  const serverCloseListener = vi.fn()
+  interceptor.once('connection', ({ server }) => {
+    server.connect()
+    server.addEventListener('close', serverCloseListener)
+  })
+
+  new WebSocket(getWsUrl(wsServer))
+
+  await vi.waitFor(() => {
+    expect(serverCloseListener).toHaveBeenCalledTimes(1)
+  })
+})
+
+it('emits "close" event when the server connection is closed by the interceptor', async () => {
+  const serverCloseListener = vi.fn()
+  interceptor.once('connection', ({ server }) => {
+    server.connect()
+    server.addEventListener('close', serverCloseListener)
+    queueMicrotask(() => server.close())
+  })
+
+  new WebSocket(getWsUrl(wsServer))
+
+  await vi.waitFor(() => {
+    expect(serverCloseListener).toHaveBeenCalledTimes(1)
+  })
+})
