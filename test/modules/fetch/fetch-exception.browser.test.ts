443262e3bc9a9c2062f40de1595feeaf7a0f2b72
Artem Zakharchenko <kettanaito@gmail.com>
2024-04-16 18:41:52 +0200

    test: reflect the change in tests

--- test/modules/fetch/fetch-exception.browser.test.ts
+++ test/modules/fetch/fetch-exception.browser.test.ts
@@ -1,22 +1,30 @@
+import { FetchInterceptor } from '../../../src/interceptors/fetch'
 import { test, expect } from '../../playwright.extend'
 
-test('treats middleware exceptions as TypeError: Failed to fetch', async ({
+declare global {
+  interface Window {
+    interceptor: FetchInterceptor
+  }
+}
+
+test('treats Response.error() mocked responses as TypeError: Failed to fetch', async ({
   loadExample,
   page,
 }) => {
   await loadExample(require.resolve('./fetch-exception.runtime.js'))
 
+  await page.evaluate(() => {
+    window.interceptor.once('request', ({ request }) => {
+      request.respondWith(Response.error())
+    })
+  })
+
   const fetchRejectionError = await page.evaluate(() => {
-    return fetch('http://localhost:3001/resource').catch(
+    return fetch('http://localhost:3000/resource').catch(
       (error: TypeError & { cause: Error }) => {
-        // Serialize the error to retrieve it in the test.
         return {
           name: error.name,
           message: error.message,
-          cause: {
-            name: error.cause.name,
-            message: error.cause.message,
-          },
         }
       }
     )
@@ -25,9 +33,35 @@ test('treats middleware exceptions as TypeError: Failed to fetch', async ({
   expect(fetchRejectionError).toEqual({
     name: 'TypeError',
     message: 'Failed to fetch',
-    cause: {
-      name: 'Error',
-      message: 'Network error',
-    },
+  })
+})
+
+test('treats unhandled interceptor exceptions as 500 error responses', async ({
+  loadExample,
+  page,
+}) => {
+  await loadExample(require.resolve('./fetch-exception.runtime.js'))
+
+  await page.evaluate(() => {
+    window.interceptor.once('request', ({ request }) => {
+      throw new Error('Server error')
+    })
+  })
+
+  const response = await page.evaluate(async () => {
+    const response = await fetch('http://localhost:3000/resource')
+    return {
+      status: response.status,
+      statusText: response.statusText,
+      json: await response.json(),
+    }
+  })
+
+  expect(response.status).toBe(500)
+  expect(response.statusText).toBe('Unhandled Exception')
+  expect(response.json).toEqual({
+    name: 'Error',
+    message: 'Server error',
+    stack: expect.any(String),
   })
 })
