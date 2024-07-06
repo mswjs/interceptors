31db9ee821bf53ff9c3e4bd11b6e929ff04ab23a
Artem Zakharchenko <kettanaito@gmail.com>
2024-07-06 15:38:38 +0200

    test: add failing test

---
+++ test/modules/http/compliance/http-response-headers-folding.test.ts
@@ -0,0 +1,74 @@
+// @vitest-environment node
+import { it, expect, beforeAll, afterAll } from 'vitest'
+import http from 'http'
+import { ClientRequestInterceptor } from '../../../../src/interceptors/ClientRequest'
+import { waitForClientRequest } from '../../../helpers'
+
+const interceptor = new ClientRequestInterceptor()
+
+beforeAll(() => {
+  interceptor.apply()
+})
+
+afterAll(() => {
+  interceptor.dispose()
+})
+
+it('preserves the original mocked response headers casing in "rawHeaders"', async () => {
+  interceptor.once('request', ({ request }) => {
+    request.respondWith(
+      new Response(null, {
+        headers: {
+          'X-CustoM-HeadeR': 'Yes',
+        },
+      })
+    )
+  })
+
+  const request = http.get('http://localhost/resource')
+  const { res } = await waitForClientRequest(request)
+
+  expect(res.rawHeaders).toStrictEqual(['X-CustoM-HeadeR', 'Yes'])
+  expect(res.headers).toStrictEqual({ 'x-custom-header': 'Yes' })
+})
+
+it('folds duplicate headers the same as Node', async () => {
+  const replyHeaders = [
+    'Content-Type',
+    'text/html; charset=utf-8',
+    'set-cookie',
+    'set-cookie1=foo',
+    'set-cookie',
+    'set-cookie2=bar',
+    'set-cookie',
+    'set-cookie3=baz',
+    'cookie',
+    'cookie1=foo; cookie2=bar',
+    'cookie',
+    'cookie3=baz',
+    'x-custom',
+    'custom1',
+    'X-Custom',
+    'custom2',
+    'X-Custom',
+    'custom3',
+  ]
+  interceptor.once('request', ({ request }) => {
+    const response = new Response(null)
+    for (let i = 0; i < replyHeaders.length; i += 2) {
+      response.headers.append(replyHeaders[i], replyHeaders[i + 1])
+    }
+    request.respondWith(response)
+  })
+
+  const request = http.get('http://localhost/resource')
+  const { res } = await waitForClientRequest(request)
+
+  expect(res.headers).toEqual({
+    'content-type': 'text/html; charset=utf-8',
+    'set-cookie': ['set-cookie1=foo', 'set-cookie2=bar', 'set-cookie3=baz'],
+    cookie: 'cookie1=foo; cookie2=bar; cookie3=baz',
+    'x-custom': 'custom1, custom2, custom3',
+  })
+  expect(res.rawHeaders).toEqual(replyHeaders)
+})
