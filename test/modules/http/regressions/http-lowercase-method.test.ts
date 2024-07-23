4a8bf709217c6fd53545878f83db8132c181b3a0
Michael Solomon <micheal540@gmail.com>
2024-07-23 22:40:10 +0300

    fix(ClientRequest): normalize http method

---
+++ test/modules/http/regressions/http-lowercase-method.test.ts
@@ -0,0 +1,25 @@
+/**
+ * @vitest-environment node
+ */
+import { it, expect, beforeAll, afterAll } from 'vitest'
+import http from 'node:http'
+import { ClientRequestInterceptor } from '../../../../src/interceptors/ClientRequest'
+import { waitForClientRequest } from '../../../helpers'
+
+const interceptor = new ClientRequestInterceptor()
+
+beforeAll(() => {
+  interceptor.apply()
+})
+
+afterAll(() => {
+  interceptor.dispose()
+})
+
+it('Lowercased get method should work', async () => {
+  const request = http.get('http://example.com', { method: 'get' })
+  const { res } = await waitForClientRequest(request)
+
+  expect(res.statusCode).toBe(200)
+})
+
