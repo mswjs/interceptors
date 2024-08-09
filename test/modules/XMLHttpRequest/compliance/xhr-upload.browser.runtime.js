472bcbe4b6b7f0652da10eb647a3aaa898ad4f7c
Artem Zakharchenko <kettanaito@gmail.com>
2024-08-09 19:38:53 +0200

    fix(XMLHttpRequest): support request `upload` events (#613)

---
+++ test/modules/XMLHttpRequest/compliance/xhr-upload.browser.runtime.js
@@ -0,0 +1,40 @@
+import { XMLHttpRequestInterceptor } from '@mswjs/interceptors/XMLHttpRequest'
+
+const interceptor = new XMLHttpRequestInterceptor()
+interceptor.apply()
+window.interceptor = interceptor
+
+window.waitForXMLHttpRequest = (xhr) => {
+  return new Promise((resolve, reject) => {
+    xhr.onloadend = resolve
+    xhr.onerror = () => reject(new Error('XMLHttpRequest errored'))
+  })
+}
+
+window.spyOnXMLHttpRequest = (xhr) => {
+  const listeners = []
+  const callbacks = []
+
+  const pushListener = ({ type, loaded, total }) => {
+    listeners.push({ type, loaded, total })
+  }
+  const pushCallback = ({ type, loaded, total }) => {
+    callbacks.push({ type, loaded, total })
+  }
+
+  xhr.upload.addEventListener('loadstart', pushListener)
+  xhr.upload.addEventListener('progress', pushListener)
+  xhr.upload.addEventListener('load', pushListener)
+  xhr.upload.addEventListener('loadend', pushListener)
+  xhr.upload.addEventListener('timeout', pushListener)
+  xhr.upload.addEventListener('error', pushListener)
+
+  xhr.upload.onloadstart = pushCallback
+  xhr.upload.onprogress = pushCallback
+  xhr.upload.onload = pushCallback
+  xhr.upload.onloadend = pushCallback
+  xhr.upload.ontimeout = pushCallback
+  xhr.upload.onerror = pushCallback
+
+  return { listeners, callbacks }
+}
