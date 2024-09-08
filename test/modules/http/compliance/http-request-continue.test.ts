2e59653a531afa304c271da4c1931e55edd26ad8
Artem Zakharchenko <kettanaito@gmail.com>
2024-09-08 17:06:15 +0200

    test: 100-expect test

--- test/modules/http/compliance/http-request-continue.test.ts
+++ test/modules/http/compliance/http-request-continue.test.ts
@@ -1,5 +1,5 @@
 // @vitest-environment node
-import { it, expect, beforeAll, afterAll } from 'vitest'
+import { vi, it, expect, beforeAll, afterEach, afterAll } from 'vitest'
 import http from 'http'
 import { HttpServer } from '@open-draft/test-server/http'
 import { ClientRequestInterceptor } from '../../../../src/interceptors/ClientRequest'
@@ -8,8 +8,13 @@ import { waitForClientRequest } from '../../../helpers'
 const interceptor = new ClientRequestInterceptor()
 
 const httpServer = new HttpServer((app) => {
-  app.get('/resource', (req, res) => {
-    res.send('original response')
+  app.post('/resource', (req, res) => {
+    req.on('data', (chunk) =>
+      console.log('[server] req data:', chunk.toString())
+    )
+    console.log('!!![server] added req.on(data)')
+
+    req.pipe(res)
   })
 })
 
@@ -18,24 +23,81 @@ beforeAll(async () => {
   await httpServer.listen()
 })
 
+afterEach(() => {
+  interceptor.removeAllListeners()
+})
+
 afterAll(async () => {
   interceptor.dispose()
   await httpServer.close()
 })
 
-it('Unmocked request with `Expect: 100-continue` triggers continue event', async () => {
-  const body = 'this is the full request body'
-  const request = http.get(httpServer.http.url('/resource'), {
-    headers: { Expect: '100-continue' },
+it('emits "continue" event for a request with "100-continue" expect header', async () => {
+  interceptor
+    .on('request', ({ request }) => {
+      console.log('[*] request', request.method, request.url)
+    })
+    .on('response', ({ request, response }) => {
+      console.log('[*] response', response.status, request.method, request.url)
+    })
+
+  const request = http.request(httpServer.http.url('/resource'), {
+    method: 'POST',
+    headers: {
+      expect: '100-continue',
+    },
   })
+
+  const continueListener = vi.fn()
+  request.on('continue', continueListener)
   request.on('continue', () => {
-    request.end(body)
+    console.log('REQ CONTINUE')
+    console.log('REQ END')
+
+    console.log('!!!! writing request...')
+    request.end('hello')
+  })
+  request.on('finish', () => console.log('REQ FINISH'))
+  request.on('response', () => console.log('REQ RESPONSE'))
+
+  request.on('socket', (socket) => {
+    socket.write = new Proxy(socket.write, {
+      apply(target, thisArg, args) {
+        console.log('SOCKET WRITE', args[0].toString())
+        return Reflect.apply(target, thisArg, args)
+      },
+    })
+    socket.push = new Proxy(socket.push, {
+      apply(target, thisArg, args) {
+        console.log('SOCKET PUSH', args[0].toString())
+        return Reflect.apply(target, thisArg, args)
+      },
+    })
+    socket.emit = new Proxy(socket.emit, {
+      apply(target, thisArg, args) {
+        console.log(
+          'SOCKET EMIT',
+          args[0] === 'data' ? ['data', args[1].toString()] : args
+        )
+        return Reflect.apply(target, thisArg, args)
+      },
+    })
+
+    socket.on('connect', () => console.log('SOCKET CONNECT'))
+    // socket.on('data', (chunk) =>
+    //   console.log('SOCKET DATA:\n', chunk.toString())
+    // )
+    socket.on('finish', () => console.log('SOCKET FINISH'))
+    socket.on('close', () => console.log('SOCKET CLOSE'))
+    socket.on('error', () => console.log('SOCKET ERROR!!!'))
+    socket.on('end', () => console.log('SOCKET END'))
   })
 
   const { res, text } = await waitForClientRequest(request)
 
   expect(res.statusCode).toBe(200)
-  expect(await text()).toBe('original response')
+  await expect(text()).resolves.toBe('hello')
+  expect(continueListener).toHaveBeenCalledOnce()
 })
 
-it.todo('Mocked request with `Expect: 100-continue` triggers continue event')
+it.todo('emits "continue" event for a ')
