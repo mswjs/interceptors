ffcd1697463fdfd6eed500af69a934d7278b9009
Artem Zakharchenko <kettanaito@gmail.com>
2025-04-04 11:32:48 +0200

    fix: set `response.url` correctly

--- src/utils/fetchUtils.ts
+++ src/utils/fetchUtils.ts
@@ -1,5 +1,26 @@
+import { getValueBySymbol } from './getValueBySymbol'
+
 export interface FetchResponseInit extends ResponseInit {
   url?: string
+}
+
+interface UndiciFetchInternalState {
+  aborted: boolean
+  rangeRequested: boolean
+  timingAllowPassed: boolean
+  requestIncludesCredentials: boolean
+  type: ResponseType
+  status: number
+  statusText: string
+  timingInfo: unknown
+  cacheState: unknown
+  headersList: Record<symbol, Map<string, unknown>>
+  urlList: Array<URL>
+  body?: {
+    stream: ReadableStream
+    source: unknown
+    length: number
+  }
 }
 
 export class FetchResponse extends Response {
@@ -28,20 +49,25 @@ export class FetchResponse extends Response {
   }
 
   static setUrl(url: string | undefined, response: Response): void {
-    if (!url) {
+    if (url == null) {
       return
     }
 
-    if (response.url != '') {
-      return
-    }
+    const state = getValueBySymbol<UndiciFetchInternalState>('state', response)
 
-    Object.defineProperty(response, 'url', {
-      value: url,
-      enumerable: true,
-      configurable: true,
-      writable: false,
-    })
+    if (state) {
+      // In Undici, push the URL to the internal list of URLs.
+      // This will respect the `response.url` getter logic correctly.
+      state.urlList.push(new URL(url))
+    } else {
+      // In other libraries, redefine the `url` property directly.
+      Object.defineProperty(response, 'url', {
+        value: url,
+        enumerable: true,
+        configurable: true,
+        writable: false,
+      })
+    }
   }
 
   /**
