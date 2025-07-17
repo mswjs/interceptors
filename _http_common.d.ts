757707417b2f7e7868d227bb48ae9a85dbf08858
Artem Zakharchenko <kettanaito@gmail.com>
2025-07-17 17:31:26 +0200

    chore(_http_common): add missing internal callback symbols

--- _http_common.d.ts
+++ _http_common.d.ts
@@ -2,21 +2,31 @@ declare var HTTPParser: {
   new (): HTTPParser<number>
   REQUEST: 0
   RESPONSE: 1
+
+  /**
+   * @see https://github.com/nodejs/node/blob/229cc3be28eab3153c16bc55bc67d1e81c4a7067/src/node_http_parser.cc#L76
+   */
+  readonly kOnMessageBegin: unique symbol
   readonly kOnHeaders: unique symbol
   readonly kOnHeadersComplete: unique symbol
   readonly kOnBody: unique symbol
   readonly kOnMessageComplete: unique symbol
+  readonly kOnExecute: unique symbol
+  readonly kOnTimeout: unique symbol
 }
 
 export interface HTTPParser<ParserType extends number> {
   new (): HTTPParser<ParserType>
 
+  [HTTPParser.kOnMessageBegin]: () => void
   [HTTPParser.kOnHeaders]: RequestHeadersCallback
   [HTTPParser.kOnHeadersComplete]: ParserType extends 0
     ? RequestHeadersCompleteCallback
     : ResponseHeadersCompleteCallback
   [HTTPParser.kOnBody]: (chunk: Buffer) => void
   [HTTPParser.kOnMessageComplete]: () => void
+  [HTTPParser.kOnExecute]: () => void
+  [HTTPParser.kOnTimeout]: () => void
 
   initialize(type: ParserType, asyncResource: object): void
   execute(buffer: Buffer): void
