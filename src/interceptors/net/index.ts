32fcd0f8122e22cfc7cef4301c0e2888dc2bd043
Artem Zakharchenko <kettanaito@gmail.com>
2025-08-05 15:05:44 +0200

    fix: patch `node:net` early; add `http` tests

--- src/interceptors/net/index.ts
+++ src/interceptors/net/index.ts
@@ -16,6 +16,46 @@ export interface SocketConnectionEventMap {
   ]
 }
 
+const kImplementation = Symbol('kImplementation')
+const kRestoreValue = Symbol('kRestoreValue')
+
+/**
+ * Apply a transparent proxy to the "node:net" module on the module's scope.
+ * This way, this interceptor can function if it gets imported before the surface
+ * that relies on "node:net", like "node:http" or "undici".
+ *
+ * @note You MUST import the interceptor BEFORE the surface relying on "node:net".
+ */
+const { createConnection } = net
+Object.defineProperties(net.createConnection, {
+  [kRestoreValue]: {
+    value: createConnection.bind(createConnection),
+    enumerable: true,
+  },
+  [kImplementation]: {
+    value() {
+      return Reflect.get(net.createConnection, kRestoreValue)
+    },
+    enumerable: true,
+    writable: true,
+  },
+})
+
+function createSwitchableProxy(target: any) {
+  return new Proxy(target, {
+    apply(target, thisArg, argArray) {
+      return Reflect.apply(
+        Reflect.get(target, kImplementation),
+        thisArg,
+        argArray
+      )
+    },
+  })
+}
+
+net.connect = createSwitchableProxy(net.connect)
+net.createConnection = createSwitchableProxy(net.createConnection)
+
 export class SocketInterceptor extends Interceptor<SocketConnectionEventMap> {
   static symbol = Symbol('SocketInterceptor')
 
@@ -24,12 +64,13 @@ export class SocketInterceptor extends Interceptor<SocketConnectionEventMap> {
   }
 
   protected setup(): void {
-    const {
-      connect: originalConnect,
-      createConnection: originalCreateConnection,
-    } = net
+    const originalConnect = Reflect.get(net.connect, kRestoreValue)
+    const originalCreateConnection = Reflect.get(
+      net.createConnection,
+      kRestoreValue
+    )
 
-    net.connect = (...args: Array<unknown>) => {
+    Reflect.set(net.connect, kImplementation, (...args: Array<unknown>) => {
       const [options, connectionListener] = normalizeNetConnectArgs(
         args as NetConnectArgs
       )
@@ -37,7 +78,7 @@ export class SocketInterceptor extends Interceptor<SocketConnectionEventMap> {
         ...args,
         onConnect: connectionListener,
         createConnection() {
-          return originalConnect.apply(originalCreateConnection, args as any)
+          return originalConnect.apply(originalConnect, args as any)
         },
       })
 
@@ -47,31 +88,39 @@ export class SocketInterceptor extends Interceptor<SocketConnectionEventMap> {
       })
 
       return socket
-    }
+    })
 
-    net.createConnection = (...args: Array<unknown>) => {
-      const [options] = normalizeNetConnectArgs(args as NetConnectArgs)
-      const socket = new MockSocket({
-        ...args,
-        createConnection() {
-          return originalCreateConnection.apply(
-            originalCreateConnection,
-            args as any
-          )
-        },
-      })
+    Reflect.set(
+      net.createConnection,
+      kImplementation,
+      (...args: Array<unknown>) => {
+        const [options] = normalizeNetConnectArgs(args as NetConnectArgs)
+        const socket = new MockSocket({
+          ...args,
+          createConnection() {
+            return originalCreateConnection.apply(
+              originalCreateConnection,
+              args as any
+            )
+          },
+        })
 
-      this.emitter.emit('connection', {
-        options,
-        socket,
-      })
+        this.emitter.emit('connection', {
+          options,
+          socket,
+        })
 
-      return socket
-    }
+        return socket
+      }
+    )
 
     this.subscriptions.push(() => {
-      net.connect = originalConnect
-      net.createConnection = originalCreateConnection
+      Reflect.set(net.connect, kImplementation, originalConnect)
+      Reflect.set(
+        net.createConnection,
+        kImplementation,
+        originalCreateConnection
+      )
     })
   }
 }
