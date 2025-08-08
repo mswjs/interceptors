d285a5e76efb0efc1d277ce79b4acfd5f737e599
Artem Zakharchenko <kettanaito@gmail.com>
2025-08-08 13:13:42 +0200

    test: use `https.globalAgent` for HTTPS `ClientRequest`

--- test/modules/http/intercept/http-client-request.test.ts
+++ test/modules/http/intercept/http-client-request.test.ts
@@ -1,6 +1,7 @@
 // @vitest-environment node
 import { HttpRequestInterceptor } from '../../../../src/interceptors/http'
 import http from 'node:http'
+import https from 'node:https'
 import { HttpServer } from '@open-draft/test-server/http'
 import { REQUEST_ID_REGEXP, waitForClientRequest } from '../../../helpers'
 import { RequestController } from '../../../../src/RequestController'
@@ -16,8 +17,8 @@ const interceptor = new HttpRequestInterceptor()
 
 beforeAll(async () => {
   process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0'
-  await httpServer.listen()
   interceptor.apply()
+  await httpServer.listen()
 })
 
 afterEach(() => {
@@ -134,7 +135,11 @@ it('intercepts an HTTPS ClientRequest request with URL string', async () => {
     vi.fn<(...args: HttpRequestEventMap['request']) => void>()
 
   interceptor.on('request', requestListener)
-  const req = new http.ClientRequest(url)
+
+  const req = new http.ClientRequest(url, {
+    // @ts-expect-error `ClientRequest` constructor signature is wrong.
+    agent: https.globalAgent,
+  })
   req.setHeader('x-custom-header', 'yes')
   req.end()
 
@@ -164,7 +169,10 @@ it('intercepts an HTTPS ClientRequest request with URL instance', async () => {
     vi.fn<(...args: HttpRequestEventMap['request']) => void>()
 
   interceptor.on('request', requestListener)
-  const req = new http.ClientRequest(url)
+  const req = new http.ClientRequest(url, {
+    // @ts-expect-error `ClientRequest` constructor signature is wrong.
+    agent: https.globalAgent,
+  })
   req.setHeader('x-custom-header', 'yes')
   req.end()
 
@@ -195,6 +203,7 @@ it('intercepts an HTTPS ClientRequest request with request options', async () =>
 
   interceptor.on('request', requestListener)
   const req = new http.ClientRequest({
+    agent: https.globalAgent,
     protocol: 'https:',
     hostname: url.hostname,
     port: url.port,
