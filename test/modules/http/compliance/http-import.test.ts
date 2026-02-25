7f43fb8e78119adaa5f8d8cad2f5ec2070d741f8
Artem Zakharchenko <kettanaito@gmail.com>
2026-02-25 18:09:09 +0100

    test: add http module import compliance tests

---
+++ test/modules/http/compliance/http-import.test.ts
@@ -0,0 +1,44 @@
+// @vitest-environment node
+import { it, expect, beforeAll, afterEach, afterAll } from 'vitest'
+import * as http from 'node:http'
+import * as https from 'node:https'
+import { HttpRequestInterceptor } from '../../../../src/interceptors/http'
+import { waitForClientRequest } from '../../../helpers'
+
+const interceptor = new HttpRequestInterceptor()
+
+beforeAll(() => {
+  interceptor.apply()
+})
+
+afterEach(() => {
+  interceptor.removeAllListeners()
+})
+
+afterAll(() => {
+  interceptor.dispose()
+})
+
+it('mocks a response to a request made via * as http import', async () => {
+  interceptor.on('request', ({ controller }) => {
+    controller.respondWith(new Response('hello world'))
+  })
+
+  const request = http.request('http://localhost/api').end()
+  const { res, text } = await waitForClientRequest(request)
+
+  expect(res.statusCode).toBe(200)
+  await expect(text()).resolves.toBe('hello world')
+})
+
+it('mocks a response to a request made via * as https import', async () => {
+  interceptor.on('request', ({ controller }) => {
+    controller.respondWith(new Response('hello world'))
+  })
+
+  const request = https.request('https://localhost/api').end()
+  const { res, text } = await waitForClientRequest(request)
+
+  expect(res.statusCode).toBe(200)
+  await expect(text()).resolves.toBe('hello world')
+})
