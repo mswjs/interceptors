654c9d17eae97812e70614c05836a122d897f2f1
Artem Zakharchenko <kettanaito@gmail.com>
2026-03-09 11:36:00 +0100

    test(xhr): fix "events" test

--- test/modules/XMLHttpRequest/features/events.test.ts
+++ test/modules/XMLHttpRequest/features/events.test.ts
@@ -1,42 +1,50 @@
 // @vitest-environment happy-dom
-import { HttpServer } from '@open-draft/test-server/http'
-import { XMLHttpRequestInterceptor } from '#/src/interceptors/XMLHttpRequest'
-import { useCors, REQUEST_ID_REGEXP } from '#/test/helpers'
+import { XMLHttpRequestInterceptor } from '#/src/interceptors/XMLHttpRequest/node'
+import { REQUEST_ID_REGEXP } from '#/test/helpers'
 import { HttpRequestEventMap } from '#/src/index'
 import { RequestController } from '#/src/RequestController'
 import { waitForXMLHttpRequest } from '#/test/setup/helpers-neutral'
+import { getTestServer } from '#/test/setup/vitest'
 
-const server = new HttpServer((app) => {
-  app.use(useCors)
-  app.get('/bypassed', (req, res) => {
-    res.status(201).set('Content-Type', 'text/plain').send('original response')
-  })
-})
-
+const server = getTestServer()
 const interceptor = new XMLHttpRequestInterceptor()
 
-interceptor.on('request', ({ request, controller }) => {
-  if (request.url.endsWith('/user')) {
-    return controller.respondWith(
-      new Response('mocked response', {
-        status: 200,
-        statusText: 'OK',
-      })
-    )
-  }
-})
-
-beforeAll(async () => {
+beforeAll(() => {
   interceptor.apply()
-  await server.listen()
 })
 
-afterAll(async () => {
+afterEach(() => {
+  interceptor.removeAllListeners()
+})
+
+afterAll(() => {
   interceptor.dispose()
-  await server.close()
 })
 
 it('emits events for a handled request', async () => {
+  interceptor.on('request', ({ request, controller }) => {
+    if (request.method === 'OPTIONS') {
+      return controller.respondWith(
+        new Response(null, {
+          headers: {
+            'access-control-allow-origin': '*',
+            'access-control-allow-methods': 'GET',
+          },
+        })
+      )
+    }
+
+    controller.respondWith(
+      new Response('mocked response', {
+        status: 200,
+        statusText: 'OK',
+        headers: {
+          'access-control-allow-origin': '*',
+        },
+      })
+    )
+  })
+
   const requestListener =
     vi.fn<(...args: HttpRequestEventMap['request']) => void>()
   const responseListener =
@@ -44,40 +52,74 @@ it('emits events for a handled request', async () => {
   interceptor.on('request', requestListener)
   interceptor.on('response', responseListener)
 
+  const url = server.http.url('/user')
   const request = new XMLHttpRequest()
-  request.open('GET', server.http.url('/user'))
+  request.open('GET', url)
   request.send()
 
   await waitForXMLHttpRequest(request)
 
-  // Must call the "request" event listener.
-  expect(requestListener).toHaveBeenCalledTimes(1)
-  const [requestParams] = requestListener.mock.calls[0]
+  /**
+   * @note XMLHttpRequest in JSDOM/HappyDOM issues a preflight OPTIONS request.
+   */
+  expect.soft(requestListener).toHaveBeenCalledTimes(2)
 
-  expect(requestParams.request).toBeInstanceOf(Request)
-  expect(requestParams.request.method).toBe('GET')
-  expect(requestParams.request.url).toBe(server.http.url('/user'))
+  // Preflight request.
+  {
+    const [{ request, requestId }] = requestListener.mock.calls[0]
 
-  expect(requestParams.requestId).toMatch(REQUEST_ID_REGEXP)
+    expect.soft(request).toBeInstanceOf(Request)
+    expect.soft(request.method).toBe('OPTIONS')
+    expect.soft(request.url).toBe(url.href)
+    expect.soft(requestId).toMatch(REQUEST_ID_REGEXP)
+  }
+
+  {
+    const [{ request, requestId }] = requestListener.mock.calls[1]
+
+    expect.soft(request).toBeInstanceOf(Request)
+    expect.soft(request.method).toBe('GET')
+    expect.soft(request.url).toBe(url.href)
+    expect.soft(requestId).toMatch(REQUEST_ID_REGEXP)
+  }
 
   // Must call the "response" event listener.
-  expect(responseListener).toHaveBeenCalledTimes(1)
-  const [responseParams] = responseListener.mock.calls[0]
+  expect(responseListener).toHaveBeenCalledTimes(2)
 
-  expect(responseParams.response).toBeInstanceOf(Response)
-  expect(responseParams.response.status).toBe(200)
-  expect(responseParams.response.statusText).toBe('OK')
-  expect(responseParams.response.headers.get('Content-Type')).toBe(
-    'text/plain;charset=UTF-8'
-  )
-  expect(responseParams.response.bodyUsed).toBe(false)
-  expect(await responseParams.response.text()).toBe('mocked response')
+  // Preflight response.
+  {
+    const [{ response, request, requestId }] = responseListener.mock.calls[0]
 
-  expect(responseParams.request).toBeInstanceOf(Request)
-  expect(responseParams.request.method).toBe('GET')
-  expect(responseParams.request.url).toBe(server.http.url('/user'))
+    expect.soft(response).toBeInstanceOf(Response)
+    expect.soft(response.status).toBe(200)
+    expect.soft(response.body).toBeNull()
 
-  expect(responseParams.requestId).toMatch(REQUEST_ID_REGEXP)
+    expect.soft(request).toBeInstanceOf(Request)
+    expect.soft(request.method).toBe('OPTIONS')
+    expect.soft(request.url).toBe(url.href)
+
+    expect.soft(requestId).toMatch(REQUEST_ID_REGEXP)
+  }
+
+  // Mocked response.
+  {
+    const [{ response, request, requestId }] = responseListener.mock.calls[1]
+
+    expect(response).toBeInstanceOf(Response)
+    expect(response.status).toBe(200)
+    expect(response.statusText).toBe('OK')
+    expect(response.headers.get('Content-Type')).toBe(
+      'text/plain;charset=UTF-8'
+    )
+    expect(response.bodyUsed).toBe(false)
+    await expect(response.text()).resolves.toBe('mocked response')
+
+    expect(request).toBeInstanceOf(Request)
+    expect(request.method).toBe('GET')
+    expect(request.url).toBe(url.href)
+
+    expect(requestId).toMatch(REQUEST_ID_REGEXP)
+  }
 })
 
 it('emits events for a bypassed request', async () => {
@@ -88,44 +130,73 @@ it('emits events for a bypassed request', async () => {
   interceptor.on('request', requestListener)
   interceptor.on('response', responseListener)
 
+  const url = server.http.url('/bypassed')
   const request = new XMLHttpRequest()
-  request.open('GET', server.http.url('/bypassed'))
+  request.open('GET', url)
   request.send()
 
   await waitForXMLHttpRequest(request)
 
-  // Must call the "request" event listener.
-  expect(requestListener).toHaveBeenCalledTimes(1)
-  const [requestParams] = requestListener.mock.calls[0]
+  expect(requestListener).toHaveBeenCalledTimes(2)
 
-  expect(requestParams.request).toBeInstanceOf(Request)
-  expect(requestParams.request.method).toBe('GET')
-  expect(requestParams.request.url).toBe(server.http.url('/bypassed'))
-  expect(requestParams.controller).toBeInstanceOf(RequestController)
+  // Preflight request.
+  {
+    const [{ request, controller, requestId }] = requestListener.mock.calls[0]
 
-  // The last argument of the request listener is the request ID.
-  expect(requestParams.requestId).toMatch(REQUEST_ID_REGEXP)
+    expect.soft(request).toBeInstanceOf(Request)
+    expect.soft(request.method).toBe('OPTIONS')
+    expect.soft(request.url).toBe(url.href)
+    expect.soft(controller).toBeInstanceOf(RequestController)
+    expect.soft(requestId).toMatch(REQUEST_ID_REGEXP)
+  }
 
-  // Must call the "response" event listener.
-  expect(responseListener).toHaveBeenCalledTimes(1)
-  const [responseParams] = responseListener.mock.calls[0]
+  {
+    const [{ request, controller, requestId }] = requestListener.mock.calls[1]
 
-  expect(responseParams.response).toBeInstanceOf(Response)
-  expect(responseParams.response.status).toBe(201)
-  // Note that Express infers status texts from the code.
-  expect(responseParams.response.statusText).toBe('Created')
-  // Express also adds whitespace between the header pairs.
-  expect(responseParams.response.headers.get('Content-Type')).toBe(
-    'text/plain; charset=utf-8'
-  )
-  expect(responseParams.response.bodyUsed).toBe(false)
-  expect(await responseParams.response.text()).toBe('original response')
+    expect.soft(request).toBeInstanceOf(Request)
+    expect.soft(request.method).toBe('GET')
+    expect.soft(request.url).toBe(url.href)
+    expect.soft(controller).toBeInstanceOf(RequestController)
+    expect.soft(requestId).toMatch(REQUEST_ID_REGEXP)
+  }
 
-  // Response listener must provide a relevant request.
-  expect(responseParams.request).toBeInstanceOf(Request)
-  expect(responseParams.request.method).toBe('GET')
-  expect(responseParams.request.url).toBe(server.http.url('/bypassed'))
+  expect(responseListener).toHaveBeenCalledTimes(2)
 
-  // The last argument of the response listener is the request ID.
-  expect(responseParams.requestId).toMatch(REQUEST_ID_REGEXP)
+  // Preflight response.
+  {
+    const [{ response, request, requestId }] = responseListener.mock.calls[0]
+
+    expect.soft(response).toBeInstanceOf(Response)
+    expect.soft(response.status).toBe(200)
+    await expect.soft(response.text()).resolves.toBe('')
+
+    expect.soft(request).toBeInstanceOf(Request)
+    expect.soft(request.method).toBe('OPTIONS')
+    expect.soft(request.url).toBe(url.href)
+
+    expect.soft(requestId).toMatch(REQUEST_ID_REGEXP)
+  }
+
+  {
+    const [responseParams] = responseListener.mock.calls[1]
+
+    expect.soft(responseParams.response).toBeInstanceOf(Response)
+    expect.soft(responseParams.response.status).toBe(200)
+    expect.soft(responseParams.response.statusText).toBe('OK')
+    expect
+      .soft(responseParams.response.headers.get('Content-Type'))
+      .toBe('text/plain; charset=utf-8')
+    expect.soft(responseParams.response.bodyUsed).toBe(false)
+    await expect
+      .soft(responseParams.response.text())
+      .resolves.toBe('original-response')
+
+    // Response listener must provide a relevant request.
+    expect.soft(responseParams.request).toBeInstanceOf(Request)
+    expect.soft(responseParams.request.method).toBe('GET')
+    expect.soft(responseParams.request.url).toBe(url.href)
+
+    // The last argument of the response listener is the request ID.
+    expect.soft(responseParams.requestId).toMatch(REQUEST_ID_REGEXP)
+  }
 })
