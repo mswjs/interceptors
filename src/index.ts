33da6277eaafa06546999b0c468a2388d6b817a7
Artem Zakharchenko <kettanaito@gmail.com>
2026-04-18 18:47:52 +0200

    fix: export `FetchRequest` publicly

--- src/index.ts
+++ src/index.ts
@@ -10,6 +10,6 @@ export {
 export { createRequestId } from './createRequestId'
 export { getCleanUrl } from './utils/getCleanUrl'
 export { encodeBuffer, decodeBuffer } from './utils/bufferUtils'
-export { FetchResponse } from './utils/fetchUtils'
+export { FetchRequest, FetchResponse } from './utils/fetchUtils'
 export { getRawRequest } from './getRawRequest'
 export { resolveWebSocketUrl } from './utils/resolveWebSocketUrl'
